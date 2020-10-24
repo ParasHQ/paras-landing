e972a8965a4b690dac4b8dc98dc783dbb2a8a06e
hdriqi <riqi.asdf@gmail.com>
2020-10-24 09:27:24 +0700

    add metatags

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import jsonp from 'jsonp'
 import { useEffect, useRef, useState } from 'react'
+import Head from 'next/head'
 
 const Card = ({ imgUrl }) => {
 	const cardRef = useRef()
@@ -149,6 +150,44 @@ export default function Home() {
 
 	return (
 		<div className="min-h-screen relative bg-primary-color">
+			<Head>
+				<title>Paras — Digital Art Cards Market</title>
+				<meta
+					name="description"
+					content="Create, Trade and Collect. All-in-one social digital art cards marketplace for creators and collectors."
+				/>
+
+				<meta
+					name="twitter:title"
+					content="Paras — Digital Art Cards Market"
+				/>
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@ParasHQ" />
+				<meta name="twitter:url" content="https://paras.id" />
+				<meta
+					name="twitter:description"
+					content="Create, Trade and Collect. All-in-one social digital art cards marketplace for creators and collectors."
+				/>
+				<meta
+					name="twitter:image"
+					content="https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-v2-twitter-card-large.png"
+				/>
+				<meta property="og:type" content="website" />
+				<meta property="og:title" content="Paras — Digital Art Cards Market" />
+				<meta
+					property="og:site_name"
+					content="Paras — Digital Art Cards Market"
+				/>
+				<meta
+					property="og:description"
+					content="Create, Trade and Collect. All-in-one social digital art cards marketplace for creators and collectors."
+				/>
+				<meta property="og:url" content="https://paras.id" />
+				<meta
+					property="og:image"
+					content="https://paras-media.s3-ap-southeast-1.amazonaws.com/paras-v2-twitter-card-large.png"
+				/>
+			</Head>
 			<div
 				className="h-screen w-full absolute"
 				style={{
