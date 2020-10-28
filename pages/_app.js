f25982122338d94a004946a575c4cec1d7433794
hdriqi <riqi.asdf@gmail.com>
2020-10-28 23:55:00 +0700

    add near-api-js and login functionality

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
+import { useEffect } from 'react'
+import near from '../lib/near'
 import '../styles/font.css'
 import '../styles/tailwind.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	useEffect(() => {
+		_init()
+	}, [])
+
+	const _init = async () => {
+		await near.init()
+	}
+
+	return <Component {...pageProps} />
 }
 
 export default MyApp
