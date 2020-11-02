d3ced896788cc0bde37ae4ba44660d2df9cddc11
hdriqi <riqi.asdf@gmail.com>
2020-11-02 14:48:12 +0700

    add update card listing modal

--- pages/token/[id].js
+++ pages/token/[id].js
@@ -9,15 +9,25 @@ import near from '../../lib/near'
 import useStore from '../../store'
 import { prettyBalance } from '../../utils/common'
 import Link from 'next/link'
+import { useForm } from 'react-hook-form'
 
 const TokenDetail = ({ token }) => {
 	const store = useStore()
 	const router = useRouter()
+	const {
+		errors,
+		register,
+		handleSubmit,
+		watch,
+		setValue,
+		getValues,
+	} = useForm()
 
 	const [listingQuantity, setListingQuantity] = useState(0)
 	const [listingPrice, setListingPrice] = useState('')
 	const [activeTab, setActiveTab] = useState('owners')
 	const [showModal, setShowModal] = useState('')
+	const [isSubmitting, setIsSubmitting] = useState(false)
 
 	const _buy = async (ownership) => {
 		//   ownerId: AccountId,
@@ -40,24 +50,30 @@ const TokenDetail = ({ token }) => {
 		}
 	}
 
-	const _updatePrice = async (ownership) => {
+	const _updatePrice = async () => {
 		// export function updateMarketData(
 		//   ownerId: AccountId,
 		//   tokenId: TokenId,
 		//   quantity: u128,
 		//   amount: u128
+
+		setIsSubmitting(true)
+
 		const params = {
 			ownerId: store.currentUser,
 			tokenId: router.query.id,
-			quantity: listingQuantity.toString(),
-			amount: parseNearAmount(listingPrice),
+			quantity: getValues('quantity'),
+			amount: parseNearAmount(getValues('amount')),
 		}
 
 		try {
 			await near.contract.updateMarketData(params)
+			setShowModal(false)
 		} catch (err) {
 			console.log(err)
 		}
+
+		setIsSubmitting(false)
 	}
 
 	const _removePrice = async (ownership) => {
@@ -78,7 +94,12 @@ const TokenDetail = ({ token }) => {
 		}
 	}
 
-	console.log(token)
+	const _getUserOwnership = (userId) => {
+		const ownership = token.ownerships.find(
+			(ownership) => ownership.ownerId === userId
+		)
+		return ownership
+	}
 
 	return (
 		<div
@@ -92,42 +113,115 @@ const TokenDetail = ({ token }) => {
 				<Modal close={(_) => setShowModal('')}>
 					<div className="max-w-sm w-full px-4 bg-white m-auto rounded-md">
 						<div
-							className="py-2"
-							onClick={(_) => setShowModal('addUpdateListing')}
+							className="py-2 cursor-pointer"
+							// onClick={(_) => setShowModal('addUpdateListing')}
 						>
-							Add/Update listing
+							Copy Link
 						</div>
 						<div
-							className="pb-2"
-							onClick={(_) => setShowModal('removeListing')}
+							className="py-2 cursor-pointer"
+							// onClick={(_) => setShowModal('addUpdateListing')}
 						>
-							Remove listing
+							Buy
+						</div>
+						<div
+							className="py-2 cursor-pointer"
+							onClick={(_) => setShowModal('addUpdateListing')}
+						>
+							Sell
 						</div>
 					</div>
 				</Modal>
 			)}
 			{showModal === 'addUpdateListing' && (
-				<Modal close={(_) => setShowModal('')}>
-					<div className="max-w-sm w-full px-4 bg-white m-auto rounded-md">
+				<Modal
+					close={(_) => setShowModal('')}
+					closeOnBgClick={false}
+					closeOnEscape={false}
+				>
+					<div className="max-w-sm w-full p-4 bg-white m-auto rounded-md">
 						<div>
-							<p>Add/update listing</p>
-							<div>
-								<label>Quantity</label>
-								<input
-									type="number"
-									value={listingQuantity}
-									onChange={(e) => setListingQuantity(e.target.value)}
-								/>
-							</div>
-							<div>
-								<label>Price</label>
-								<input
-									type="string"
-									value={listingPrice}
-									onChange={(e) => setListingPrice(e.target.value)}
-								/>
-							</div>
-							<button onClick={_updatePrice}>Update Listing</button>
+							<h1 className="text-2xl font-bold text-gray-900 tracking-tight">
+								Card Listing
+							</h1>
+							<form onSubmit={handleSubmit(_updatePrice)}>
+								<div className="mt-4">
+									<label className="block text-sm">
+										Sale quantity (Owned:{' '}
+										{_getUserOwnership(store.currentUser)
+											? _getUserOwnership(store.currentUser).quantity
+											: 0}
+										)
+									</label>
+									<input
+										type="number"
+										name="quantity"
+										ref={register({
+											required: true,
+											min: 0,
+											max: _getUserOwnership(store.currentUser)
+												? _getUserOwnership(store.currentUser).quantity
+												: 0,
+										})}
+										className={`${errors.quantity && 'error'}`}
+										placeholder="Number of card on sale"
+									/>
+									<div className="mt-2 text-sm text-red-500">
+										{errors.quantity?.type === 'required' &&
+											`Sale quantity is required`}
+										{errors.quantity?.type === 'min' && `Minimum 0`}
+										{errors.quantity?.type === 'max' &&
+											`Must be less than owned`}
+									</div>
+								</div>
+								<div className="mt-4">
+									<label className="block text-sm">Sale price</label>
+									<div
+										className={`flex justify-between bg-gray-300 p-2 rounded-md focus:bg-gray-100 border-2 border-transparent focus:border-dark-primary-1 w-full ${
+											errors.amount && 'error'
+										}`}
+									>
+										<input
+											type="number"
+											name="amount"
+											ref={register({
+												required: true,
+												min: 0,
+											})}
+											className="clear pr-2"
+											placeholder="Card price per pcs"
+										/>
+										<div className="inline-block">Ⓝ</div>
+									</div>
+									<p>
+										~$
+										{Number(
+											store.nearUsdPrice * (watch('amount') || 0)
+										).toPrecision(6)}
+									</p>
+									<div className="mt-2 text-sm text-red-500">
+										{errors.amount?.type === 'required' &&
+											`Sale price is required`}
+										{errors.amount?.type === 'min' && `Minimum 0`}
+									</div>
+								</div>
+								<div className="">
+									<button
+										disabled={isSubmitting}
+										className="w-full outline-none h-12 mt-4 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-2 border-primary bg-primary text-white"
+										type="submit"
+									>
+										{!isSubmitting ? 'Create' : 'Creating...'}
+									</button>
+									<button
+										disabled={isSubmitting}
+										className="w-full outline-none h-12 mt-4 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-2 border-primary text-primary"
+										onClick={(_) => setShowModal(false)}
+									>
+										Cancel
+									</button>
+								</div>
+							</form>
 						</div>
 					</div>
 				</Modal>
@@ -180,15 +274,15 @@ const TokenDetail = ({ token }) => {
 							>
 								<path
 									d="M20 12C20 13.1046 19.1046 14 18 14C16.8954 14 16 13.1046 16 12C16 10.8954 16.8954 10 18 10C19.1046 10 20 10.8954 20 12Z"
-									fill="#E2E2E2"
+									fill="rbg(0,0,0)"
 								/>
 								<path
 									d="M14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12Z"
-									fill="#E2E2E2"
+									fill="rbg(0,0,0)"
 								/>
 								<path
 									d="M8 12C8 13.1046 7.10457 14 6 14C4.89543 14 4 13.1046 4 12C4 10.8954 4.89543 10 6 10C7.10457 10 8 10.8954 8 12Z"
-									fill="#E2E2E2"
+									fill="rbg(0,0,0)"
 								/>
 							</svg>
 						</div>
