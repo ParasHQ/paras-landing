0d546976fe72b265d44865ec13c5e7af966e1047
hdriqi <riqi.asdf@gmail.com>
2020-11-04 13:49:39 +0700

    remove ackee

--- pages/_document.js
+++ pages/_document.js
@@ -10,12 +10,6 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<script
-						async
-						src="https://stat.paras.id/tracker.js"
-						data-ackee-server="https://stat.paras.id"
-						data-ackee-domain-id="a0e57436-7219-4936-a292-8d63a9a9b9e6"
-					></script>
 				</Head>
 				<body>
 					<Main />
