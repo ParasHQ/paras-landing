68ba23e6641b0448e00f744d29506b4c2f64d64a
hdriqi <riqi.asdf@gmail.com>
2020-11-06 10:56:39 +0700

    add hooks to tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
 		removeDeprecatedGapUtilities: true,
 		purgeLayersByDefault: true,
 	},
-	purge: ['./pages/**/*.js', './components/**/*.js'],
+	purge: ['./pages/**/*.js', './components/**/*.js', './hooks/**/*.js'],
 	theme: {
 		fontFamily: {
 			body: ['Anybody', 'sans-serif'],
