709c2b426a2b2f4eb019c59c9414597b4b12b062
hdriqi <riqi.asdf@gmail.com>
2020-11-17 20:47:37 +0700

    update gtag

--- pages/_document.js
+++ pages/_document.js
@@ -26,13 +26,11 @@ class MyDocument extends Document {
 							<script
 								dangerouslySetInnerHTML={{
 									__html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-
-                    gtag('config', '${GA_TRACKING_ID}', {
-                      page_path: window.location.pathname,
-                    });
+									window.dataLayer = window.dataLayer || [];
+									function gtag(){dataLayer.push(arguments);}
+									gtag('js', new Date());
+								
+									gtag('config', '${GA_TRACKING_ID}');
                   `,
 								}}
 							/>
