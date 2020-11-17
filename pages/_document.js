cb908a639fa722aadd4eba69e4fa3f00b85edf40
hdriqi <riqi.asdf@gmail.com>
2020-11-17 20:41:21 +0700

    fix fragment not imported

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Fragment } from 'react'
 import { GA_TRACKING_ID } from '../lib/gtag'
 
 class MyDocument extends Document {
