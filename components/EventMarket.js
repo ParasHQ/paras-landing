457705b73a6749ed43e1ca5838340e16ec06f2a3
hdriqi <riqi.asdf@gmail.com>
2021-02-05 11:13:34 +0700

    update css position

--- components/EventMarket.js
+++ components/EventMarket.js
@@ -5,31 +5,33 @@ const EventMarket = ({ data }) => {
 	const router = useRouter()
 
 	return (
-		<div className="lg:flex items-center border-2 border-dashed rounded-md border-red-600 m-4">
-			<div className="w-full lg:w-2/5 p-4">
-				<h1 className="text-4xl font-bold text-yellow-400 ml-4">
+		<div className="lg:flex items-center border-2 border-dashed rounded-md border-gray-800 m-4">
+			<div className="w-full lg:w-2/5 p-8">
+				<h1 className="text-4xl font-bold text-yellow-400">
 					Lunar New Year 2021
 				</h1>
-				<p className="m-4 mt-2 text-white text-xl">
-					Explore card from Lunar 21 collection. Win total prize of 1500 Ⓝ.
+				<p className="mt-4 text-white text-xl">
+					Explore card from Lunar 21 collection and win total prize of 1500 Ⓝ
 				</p>
-				<div className="space-x-8 m-4">
+				<div className="flex flex-wrap">
 					<button
-						className="outline-none h-10 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-0 text-gray-100 bg-red-600 border-red-600 leading-relaxed"
+						className="mt-4 mr-8 outline-none h-10 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-0 text-gray-100 bg-red-600 border-red-600 leading-relaxed"
 						onClick={() => router.push('market/lunar-new-year-2021')}
 					>
 						View Collection
 					</button>
-					<a
-						className="text-gray-200 hover:text-white font-semibold border-b-2 cursor-pointer"
-						onClick={() =>
-							window.open(
-								'https://medium.com/paras-media/paras-lunar-new-year-ab1197074abd'
-							)
-						}
-					>
-						Learn more
-					</a>
+					<div className="flex items-center">
+						<a
+							className="mt-4 text-gray-200 hover:text-white font-semibold border-b-2 cursor-pointer"
+							onClick={() =>
+								window.open(
+									'https://medium.com/paras-media/paras-lunar-new-year-ab1197074abd'
+								)
+							}
+						>
+							Learn more
+						</a>
+					</div>
 				</div>
 			</div>
 			<div className="hidden w-full lg:w-3/5 md:flex flex-wrap select-none ">
