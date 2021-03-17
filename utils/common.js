a3b52f458faffb82097a46e6c117a9cb332a9ed8
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-03-17 08:04:46 +0700

    embed video publication

--- utils/common.js
+++ utils/common.js
@@ -135,3 +135,16 @@ export const compressImg = (file) => {
 		})
 	})
 }
+
+export const checkUrl = (str) => {
+	var pattern = new RegExp(
+		'^(https?:\\/\\/)?' +
+			'((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+			'((\\d{1,3}\\.){3}\\d{1,3}))' +
+			'(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+			'(\\?[;&a-z\\d%_.~+=-]*)?' +
+			'(\\#[-a-z\\d_]*)?$',
+		'i'
+	)
+	return !!pattern.test(str)
+}
