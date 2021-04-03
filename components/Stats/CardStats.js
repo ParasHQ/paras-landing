738906853bfb52100b2ca13988cb3220a75819a0
Ahnaf Alfariza <ahnaf.alfariza@gmail.com>
2021-04-03 15:51:52 +0700

    update

--- components/Stats/CardStats.js
+++ components/Stats/CardStats.js
@@ -1,18 +1,36 @@
 import Link from 'next/link'
-import React from 'react'
+import { useRouter } from 'next/router'
+import React, { useState } from 'react'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { parseImgUrl, prettyBalance } from '../../utils/common'
 import Card from '../Card'
+import CardDetailModal from '../CardDetailModal'
 import LinkToProfile from '../LinkToProfile'
 
 const CardStats = ({ cardsData, fetchData, hasMore }) => {
+	const [token, setToken] = useState(null)
+	const router = useRouter()
+
+	const onPressCard = (localToken) => {
+		setToken(localToken)
+		router.push(
+			{
+				pathname: router.pathname,
+				query: {
+					...router.query,
+					...{ tokenId: localToken?.tokenId },
+					...{ prevAs: router.asPath },
+				},
+			},
+			`/token/${localToken?.tokenId}`,
+			{ shallow: true }
+		)
+	}
+
 	return (
 		<div>
-			<InfiniteScroll
-				dataLength={cardsData.length}
-				next={fetchData}
-				hasMore={hasMore}
-			>
+			<CardDetailModal tokens={[token]} />
+			<InfiniteScroll dataLength={cardsData.length} next={fetchData} hasMore={hasMore}>
 				<table className="text-white text-center">
 					<thead>
 						<tr>
@@ -28,71 +46,65 @@ const CardStats = ({ cardsData, fetchData, hasMore }) => {
 								<p className="mx-4">Last price</p>
 							</th>
 							<th className="md:w-1/12">
-								<p className="mx-4">Average</p>
+								<p className="mx-4">Average price</p>
 							</th>
 							<th className="md:w-1/12">
-								<p className="mx-4">Total</p>
+								<p className="mx-4">Volume</p>
 							</th>
 							<th className="md:w-1/12">
-								<p className="mx-4">Transaction</p>
+								<p className="mx-4">Total card sold</p>
 							</th>
 							<th className="md:w-1/12">
-								<p className="mx-4">Change</p>
+								<p className="mx-4">Total sales</p>
 							</th>
 						</tr>
 					</thead>
 					<tbody>
 						{cardsData.map((card) => {
 							const localToken = card.token
-							const firstPrice = parseFloat(
-								prettyBalance(card.firstSale.amount, 24, 6)
-							)
-							const lastPrice = parseFloat(
-								prettyBalance(card.lastSale.amount, 24, 6)
-							)
-							const { txLength } = card
+							const firstPrice = parseFloat(prettyBalance(card.firstSale.amount, 24, 6)) || 1
+							const lastPrice = parseFloat(prettyBalance(card.lastSale.amount, 24, 6))
+							const { txLength, totalSales } = card
 							const { supply } = localToken
 							const average = prettyBalance(card.average, 24, 6)
 							const total = prettyBalance(card.volume, 24, 6)
-							const change = parseInt(
-								((lastPrice - firstPrice) / firstPrice) * 100
-							)
+							const changeLastPrice = parseInt(((lastPrice - firstPrice) / firstPrice) * 100)
+							const changeAveragePrice = parseInt(((average - firstPrice) / firstPrice) * 100)
 
 							return (
 								<tr key={card._id}>
 									<td>
-										<div className="p-4">
-											<Link href={`/token/${localToken.tokenId}`}>
-												<div className="w-32 md:w-full">
-													<Card
-														imgUrl={parseImgUrl(localToken?.metadata?.image)}
-														imgBlur={localToken?.metadata?.blurhash}
-														token={{
-															name: localToken?.metadata?.name,
-															collection: localToken?.metadata?.collection,
-															description: localToken?.metadata?.description,
-															creatorId: localToken?.creatorId,
-															supply: localToken?.supply,
-															tokenId: localToken?.tokenId,
-															createdAt: localToken?.createdAt,
-														}}
-														initialRotate={{
-															x: 0,
-															y: 0,
-														}}
-														disableFlip
-														borderRadius={'5px'}
-													/>
-												</div>
-											</Link>
+										<div className="p-4" onClick={() => onPressCard(localToken)}>
+											<div className="w-32 md:w-full">
+												<Card
+													imgUrl={parseImgUrl(localToken?.metadata?.image)}
+													imgBlur={localToken?.metadata?.blurhash}
+													token={{
+														name: localToken?.metadata?.name,
+														collection: localToken?.metadata?.collection,
+														description: localToken?.metadata?.description,
+														creatorId: localToken?.creatorId,
+														supply: localToken?.supply,
+														tokenId: localToken?.tokenId,
+														createdAt: localToken?.createdAt,
+													}}
+													initialRotate={{
+														x: 0,
+														y: 0,
+													}}
+													disableFlip
+													borderRadius={'5px'}
+												/>
+											</div>
 										</div>
 									</td>
 									<td className="text-left">
-										<Link href={`/token/${localToken.tokenId}`}>
-											<a className="text-xl md:text-2xl font-bold tracking-tight pr-4 cursor-pointer select-none">
-												{localToken.metadata.name}
-											</a>
-										</Link>
+										<a
+											onClick={() => onPressCard(localToken)}
+											className="text-xl md:text-2xl font-bold tracking-tight pr-4 cursor-pointer select-none"
+										>
+											{localToken.metadata.name}
+										</a>
 										<p className="mb-4">{localToken.metadata.collection}</p>
 										<div>
 											by{' '}
@@ -104,11 +116,21 @@ const CardStats = ({ cardsData, fetchData, hasMore }) => {
 									</td>
 									<td className="text-lg font-bold">{supply}</td>
 									<td className="text-lg font-bold">{firstPrice} Ⓝ</td>
-									<td className="text-lg font-bold">{lastPrice} Ⓝ</td>
-									<td className="text-lg font-bold">{average} Ⓝ</td>
+									<td>
+										<p className="text-lg font-bold">{lastPrice} Ⓝ</p>
+										<p className={changeLastPrice > 0 ? 'text-green-400' : 'text-red-600'}>
+											{changeLastPrice}%
+										</p>
+									</td>
+									<td>
+										<p className="text-lg font-bold">{average} Ⓝ</p>
+										<p className={changeAveragePrice > 0 ? 'text-green-400' : 'text-red-600'}>
+											{changeAveragePrice}%
+										</p>
+									</td>
 									<td className="text-lg font-bold">{total} Ⓝ</td>
 									<td className="text-lg font-bold">{txLength}</td>
-									<td className="text-lg font-bold">{change}%</td>
+									<td className="text-lg font-bold">{totalSales}</td>
 								</tr>
 							)
 						})}
