1988dce2e4a291ffaada16ebb56282db52b792f8
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-05-18 11:50:27 +0700

    backend integration

--- components/PublicationPreviewMini.js
+++ components/PublicationPreviewMini.js
@@ -1,52 +1,83 @@
+import { useEffect, useState } from 'react'
+import Axios from 'axios'
 import Link from 'next/link'
-import { parseImgUrl } from '../utils/common'
-import LinkToProfile from './LinkToProfile'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 const PublicationPreviewMini = ({ data }) => {
 	return (
 		<div className="m-auto border-2 border-dashed my-4 p-2 rounded-md">
-			{/* <div className="mb-4">
-				<div className="w-24 h-auto flex overflow-hidden">
-					<Link href={`/publication/${data.type}/${data.slug}-${data._id}`}>
-						<div className="m-auto cursor-pointer">
-							<img
-								className="w-24 object-contain"
-								src={parseImgUrl(data.thumbnail)}
-							/>
-						</div>
-					</Link>
-				</div>
-			</div> */}
 			<div className="m-auto">
 				<Link href={`/publication/${data.type}/${data.slug}-${data._id}`}>
 					<div className="cursor-pointer">
-						<h1 className="font-bold border-b-2 border-transparent">
-							{data.title}
-						</h1>
+						<h1 className="font-bold">{data.title}</h1>
 						<p className="text-sm mt-1 h-10 overflow-hidden">
 							{data.description}
 						</p>
 					</div>
 				</Link>
-				{/* <div className="mt-2 flex m-auto">
-					<p>
-						<span className="capitalize">
-							<Link href={`/publication/${data.type}`}>
-								<a className="font-bold hover:border-white border-b-2 border-transparent">
-									{data.type}
-								</a>
-							</Link>
-						</span>
-						<span className="px-2">|</span>
-						<LinkToProfile
-							accountId={data.authorId}
-							className="font-bold hover:border-white"
-						/>
-					</p>
-				</div> */}
 			</div>
 		</div>
 	)
 }
 
-export default PublicationPreviewMini
+const PublicationPreviewList = ({ tokenId }) => {
+	const [publicationList, setPublicationList] = useState([])
+	const [page, setPage] = useState(0)
+	const [isFetching, setIsFetching] = useState(false)
+	const [hasMore, setHasMore] = useState(true)
+
+	useEffect(() => {
+		if (publicationList.length === 0 && hasMore) {
+			_fetchData()
+		}
+	}, [])
+
+	const _fetchData = async () => {
+		if (!hasMore || isFetching) {
+			return
+		}
+
+		setIsFetching(true)
+		const res = await Axios(
+			`${process.env.API_URL}/publications?tokenId=${tokenId}&__skip=${
+				page * 5
+			}&__limit=5&__view=simple`
+		)
+		const newData = await res.data.data
+
+		const newPublicationList = [...publicationList, ...newData.results]
+		setPublicationList(newPublicationList)
+		setPage(page + 1)
+		if (newData.results.length === 0) {
+			setHasMore(false)
+		} else {
+			setHasMore(true)
+		}
+		setIsFetching(false)
+	}
+
+	return (
+		<div>
+			{publicationList.length === 0 && (
+				<div className="border-2 border-dashed my-4 p-2 rounded-md text-center">
+					<p className="text-gray-300 py-8 px-8">
+						Card not mentioned in any publication
+					</p>
+				</div>
+			)}
+			<InfiniteScroll
+				dataLength={publicationList.length}
+				next={_fetchData}
+				hasMore={hasMore}
+				loader={<h4>Loading...</h4>}
+				scrollableTarget="publicationListScroll"
+			>
+				{publicationList.map((pub) => {
+					return <PublicationPreviewMini key={pub._id} data={pub} />
+				})}
+			</InfiniteScroll>
+		</div>
+	)
+}
+
+export default PublicationPreviewList
