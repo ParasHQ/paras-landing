731db81f17bfb86c3c8b9bbb438e40954294f6a0
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-06-19 07:27:58 +0700

    work in progress

--- components/BidList.js
+++ components/BidList.js
@@ -1,18 +1,20 @@
 import { useEffect, useState } from 'react'
 import Axios from 'axios'
 import LinkToProfile from './LinkToProfile'
-import InfiniteScroll from 'react-infinite-scroll-component'
-import { prettyBalance, timeAgo } from '../utils/common'
+import { parseImgUrl, prettyBalance, timeAgo } from '../utils/common'
 import useSWR from 'swr'
 import Link from 'next/link'
 import useStore from '../store'
 import Modal from './Modal'
 import near from '../lib/near'
+import PlaceBidModal from './PlaceBidModal'
+import JSBI from 'jsbi'
+import { formatNearAmount, parseNearAmount } from 'near-api-js/lib/utils/format'
 
-const BidItem = ({ data, userOwnership, tokenId }) => {
-	console.log(userOwnership)
+const BidItem = ({ data, userOwnership, token, fetchBid }) => {
 	const store = useStore()
 	const [showModal, setShowModal] = useState('')
+	const [isLoading, setIsLoading] = useState(false)
 
 	const fetcher = async (key) => {
 		const resp = await axios.get(`${process.env.API_URL}/${key}`)
@@ -31,20 +33,81 @@ const BidItem = ({ data, userOwnership, tokenId }) => {
 	const acceptBid = async () => {
 		const params = {
 			ownerId: data.accountId,
-			tokenId: tokenId,
+			tokenId: token.tokenId,
 			quantity:
 				data.bidMarketData.quantity > userOwnership.quantity
 					? userOwnership.quantity.toString()
 					: data.bidMarketData.quantity.toString(),
 		}
-
-		console.log('params', params)
-
+		setIsLoading(true)
 		try {
 			await near.contract.acceptBidMarketData(params, '50000000000000')
-			console.log('success')
+			setIsLoading(false)
+			fetchBid()
 		} catch (err) {
 			console.log(err)
+			setIsLoading(false)
+		}
+	}
+
+	const updateBid = async (data) => {
+		const params = {
+			ownerId: store.currentUser,
+			tokenId: token.tokenId,
+			quantity: data.bidQuantity,
+			amount: parseNearAmount(data.bidAmount),
+		}
+
+		const attachedDeposit = JSBI.multiply(
+			JSBI.BigInt(data.bidQuantity),
+			JSBI.BigInt(parseNearAmount(data.bidAmount))
+		)
+
+		if (
+			JSBI.lessThan(JSBI.BigInt(store.userBalance.available), attachedDeposit)
+		) {
+			toast.show({
+				text: (
+					<div className="font-semibold text-center text-sm">
+						Insufficient Balance
+						<p className="mt-2">
+							Available {prettyBalance(store.userBalance.available, 24, 6)} Ⓝ
+						</p>
+					</div>
+				),
+				type: 'error',
+				duration: 2500,
+			})
+			setIsLoading(false)
+			return
+		}
+
+		setIsLoading(true)
+		await cancelBid(false)
+		try {
+			await near.contract.addBidMarketData(
+				params,
+				'50000000000000',
+				attachedDeposit.toString()
+			)
+		} catch (err) {
+			console.log(err)
+		}
+	}
+
+	const cancelBid = async (fetchAfterCancel = true) => {
+		const params = {
+			ownerId: data.accountId,
+			tokenId: token.tokenId,
+		}
+		setIsLoading(true)
+		try {
+			await near.contract.deleteBidMarketData(params, '50000000000000')
+			fetchAfterCancel && setIsLoading(false)
+			fetchAfterCancel && fetchBid()
+		} catch (err) {
+			console.log(err)
+			setIsLoading(false)
 		}
 	}
 
@@ -52,9 +115,95 @@ const BidItem = ({ data, userOwnership, tokenId }) => {
 		<>
 			{showModal === 'acceptBid' && (
 				<Modal close={(_) => setShowModal('')}>
-					<div className="max-w-sm w-full px-4 py-2 bg-gray-100 m-auto rounded-md">
-						<div>You will get 10 Near</div>
-						<div onClick={acceptBid}>Accept</div>
+					<div className="max-w-sm w-full p-4 bg-gray-100 m-auto rounded-md">
+						<div>
+							<h1 className="text-2xl font-bold text-gray-900 tracking-tight">
+								Accept a Bid
+							</h1>
+							<p className="text-gray-900 mt-2">
+								You are about to accept bid for <b>{token.metadata.name}</b>{' '}
+								from <b>ahnaf.near</b>
+							</p>
+							<div className="text-gray-900 mt-4 text-2xl font-bold text-center">
+								{`${prettyBalance(data.bidMarketData.amount, 24, 4)} Ⓝ `}
+								<span className="font-normal text-base">for </span>
+								{`${
+									data.bidMarketData.quantity > userOwnership.quantity
+										? userOwnership.quantity
+										: data.bidMarketData.quantity
+								} pcs`}
+							</div>
+							<div className="mt-4 text-center">
+								<div className="flex justify-between">
+									<div className="text-sm">Service Fee</div>
+									<div>0.5 Ⓝ</div>
+								</div>
+								<div className="flex justify-between">
+									<div className="text-sm">You will get</div>
+									<div>
+										{formatNearAmount(data.bidMarketData.amount) *
+											data.bidMarketData.quantity >
+										userOwnership.quantity
+											? userOwnership.quantity
+											: data.bidMarketData.quantity}{' '}
+										Ⓝ
+									</div>
+								</div>
+							</div>
+							<div className="">
+								<button
+									disabled={isLoading}
+									className="w-full outline-none h-12 mt-4 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-2 border-primary bg-primary text-gray-100"
+									onClick={acceptBid}
+								>
+									{isLoading ? 'Accepting...' : 'Accept Bid'}
+								</button>
+								<button
+									disabled={isLoading}
+									className="w-full outline-none h-12 mt-4 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-2 border-primary text-primary"
+									onClick={() => setShowModal('')}
+								>
+									Cancel
+								</button>
+							</div>
+						</div>
+					</div>
+				</Modal>
+			)}
+			{showModal === 'updateBid' && (
+				<PlaceBidModal
+					bidAmount={prettyBalance(data.bidMarketData.amount, 24, 4)}
+					bidQuantity={data.bidMarketData.quantity}
+					isSubmitting={isLoading}
+					localToken={token}
+					onCancel={() => setShowModal('')}
+					onSubmitForm={updateBid}
+				/>
+			)}
+			{showModal === 'cancelBid' && (
+				<Modal
+					close={() => setShowModal('')}
+					closeOnBgClick={false}
+					closeOnEscape={false}
+				>
+					<div className="w-full max-w-xs p-4 m-auto bg-gray-100 rounded-md overflow-y-auto max-h-screen">
+						<div className="w-full">Are you sure to delete your bids?</div>
+						<div className="flex space-x-4">
+							<button
+								disabled={isLoading}
+								className="w-full outline-none h-12 mt-4 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-2 border-primary bg-primary text-gray-100"
+								onClick={cancelBid}
+							>
+								{isLoading ? 'Deleting...' : 'Delete'}
+							</button>
+							<button
+								disabled={isLoading}
+								className="w-full outline-none h-12 mt-4 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-2 border-primary bg-white text-primary"
+								onClick={() => setShowModal('')}
+							>
+								Cancel
+							</button>
+						</div>
 					</div>
 				</Modal>
 			)}
@@ -91,9 +240,33 @@ const BidItem = ({ data, userOwnership, tokenId }) => {
 						</button>
 					)}
 					{store.currentUser === data.accountId && (
-						<button className="font-semibold w-24 rounded-md border-primary border-2 text-primary">
-							Cancel
-						</button>
+						<div className="flex space-x-1">
+							<button
+								onClick={() => setShowModal('updateBid')}
+								className="font-semibold w-24 rounded-md border-primary border-2 text-primary"
+							>
+								Update
+							</button>
+							<div
+								onClick={() => setShowModal('cancelBid')}
+								className="border-2 border-red-700 px-1 flex items-center justify-center rounded-md"
+							>
+								<svg
+									width="16"
+									height="16"
+									viewBox="0 0 16 16"
+									fill="none"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										fillRule="evenodd"
+										clipRule="evenodd"
+										d="M8.00008 9.41423L3.70718 13.7071L2.29297 12.2929L6.58586 8.00001L2.29297 3.70712L3.70718 2.29291L8.00008 6.5858L12.293 2.29291L13.7072 3.70712L9.41429 8.00001L13.7072 12.2929L12.293 13.7071L8.00008 9.41423Z"
+										fill="#c53030"
+									/>
+								</svg>
+							</div>
+						</div>
 					)}
 				</div>
 			</div>
@@ -101,71 +274,48 @@ const BidItem = ({ data, userOwnership, tokenId }) => {
 	)
 }
 
-const BidList = ({ tokenId, userOwnership }) => {
+const BidList = ({ userOwnership, token }) => {
 	const [bidList, setBidList] = useState([])
-	const [page, setPage] = useState(0)
-	const [isFetching, setIsFetching] = useState(false)
-	const [hasMore, setHasMore] = useState(true)
+	const [isFetching, setIsFetching] = useState(true)
 
 	useEffect(() => {
-		if (bidList.length === 0 && hasMore) {
-			_fetchData()
-		}
+		_fetchData()
 	}, [])
 
 	const _fetchData = async () => {
-		if (!hasMore || isFetching) {
-			return
-		}
-
-		setIsFetching(true)
 		const res = await Axios(
-			`${process.env.API_URL}/bids?tokenId=${tokenId}&__skip=${
-				page * 5
-			}&__limit=5&`
+			`${process.env.API_URL}/bids?tokenId=${token.tokenId}`
 		)
 		const newData = await res.data.data
 
-		const newBidList = [...bidList, ...newData.results]
-		setBidList(newBidList)
-		setPage(page + 1)
-		if (newData.results.length === 0) {
-			setHasMore(false)
-		} else {
-			setHasMore(true)
-		}
+		setBidList(newData.results)
 		setIsFetching(false)
 	}
 
 	return (
 		<div>
-			{bidList.length === 0 && !hasMore && (
+			{bidList.length === 0 && !isFetching && (
 				<div className="border-2 border-dashed my-4 p-2 rounded-md text-center">
 					<p className="text-gray-500 py-8 px-8">No bidding yet</p>
 				</div>
 			)}
-			<InfiniteScroll
-				dataLength={bidList.length}
-				next={_fetchData}
-				hasMore={hasMore}
-				loader={
-					<div className="border-2 border-dashed my-4 p-2 rounded-md text-center">
-						<p className="my-2 text-center">Loading...</p>
-					</div>
-				}
-				scrollableTarget="BidList"
-			>
-				{bidList.map((bid) => {
+			{isFetching && (
+				<div className="border-2 border-dashed my-4 p-2 rounded-md text-center">
+					<p className="my-2 text-center">Loading...</p>
+				</div>
+			)}
+			{bidList.length !== 0 &&
+				bidList.map((bid) => {
 					return (
 						<BidItem
 							key={bid._id}
 							data={bid}
 							userOwnership={userOwnership}
-							tokenId={tokenId}
+							token={token}
+							fetchBid={_fetchData}
 						/>
 					)
 				})}
-			</InfiniteScroll>
 		</div>
 	)
 }
--- components/CardDetail.js
+++ components/CardDetail.js
@@ -27,6 +27,7 @@ import PublicationPreviewMini from './PublicationPreviewMini'
 import TokenInfoCopy from './TokenInfoCopy'
 import BidList from './BidList'
 import { useRouter } from 'next/router'
+import PlaceBidModal from './PlaceBidModal'
 
 const Activity = ({ activity }) => {
 	if (activity.type === 'marketUpdate') {
@@ -403,6 +404,25 @@ const CardDetail = ({ token }) => {
 			JSBI.BigInt(data.bidQuantity),
 			JSBI.BigInt(parseNearAmount(data.bidAmount))
 		)
+
+		if (
+			JSBI.lessThan(JSBI.BigInt(store.userBalance.available), attachedDeposit)
+		) {
+			toast.show({
+				text: (
+					<div className="font-semibold text-center text-sm">
+						Insufficient Balance
+						<p className="mt-2">
+							Available {prettyBalance(store.userBalance.available, 24, 6)} Ⓝ
+						</p>
+					</div>
+				),
+				type: 'error',
+				duration: 2500,
+			})
+			setIsSubmitting(false)
+			return
+		}
 
 		try {
 			await near.contract.addBidMarketData(
@@ -1209,107 +1229,12 @@ const CardDetail = ({ token }) => {
 				</Modal>
 			)}
 			{showModal === 'placeBid' && (
-				<Modal
-					close={(_) => setShowModal('')}
-					closeOnBgClick={false}
-					closeOnEscape={false}
-				>
-					<div className="max-w-sm w-full p-4 bg-gray-100 m-auto rounded-md">
-						<div>
-							<h1 className="text-2xl font-bold text-gray-900 tracking-tight">
-								Place a Bid
-							</h1>
-							<p className="text-gray-900 mt-2">
-								You are about to bid <b>{localToken.metadata.name}</b>. You must
-								bid at least <b>0.15 Ⓝ</b>
-							</p>
-							<form onSubmit={handleSubmit(_placebid)}>
-								<div className="mt-4 flex space-x-4">
-									<div className="w-1/2">
-										<label className="block text-sm">Quantity</label>
-										<input
-											type="number"
-											name="bidQuantity"
-											ref={register({
-												required: true,
-												min: 1,
-												max: localToken.supply,
-											})}
-											className={`${errors.bidQuantity && 'error'}`}
-											placeholder="Bid quantity"
-										/>
-										<div className="mt-2 text-sm text-red-500">
-											{errors.bidQuantity?.type === 'required' &&
-												`Bid quantity is required`}
-											{errors.bidQuantity?.type === 'min' && `Minimum 1`}
-											{errors.bidQuantity?.type === 'max' &&
-												`Must be less than number of supply`}
-										</div>
-									</div>
-									<div className="w-1/2">
-										<label className="block text-sm">Amount in Ⓝ</label>
-										<input
-											type="number"
-											name="bidAmount"
-											ref={register({
-												required: true,
-												min: 1,
-											})}
-											className={`${errors.bidAmount && 'error'}`}
-											placeholder="Place your bid"
-										/>
-										<div className="mt-2 text-sm text-red-500">
-											{errors.bidAmount?.type === 'required' &&
-												`Buy quantity is required`}
-											{errors.bidAmount?.type === 'min' && `Minimum 1 Ⓝ`}
-										</div>
-									</div>
-								</div>
-								<div className="mt-4 text-center">
-									<div className="flex justify-between">
-										<div className="text-sm">Your balance</div>
-										<div>
-											{prettyBalance(store.userBalance.available, 24, 4)} Ⓝ
-										</div>
-									</div>
-									<div className="flex justify-between">
-										<div className="text-sm">Service Fee</div>
-										<div>0.5 Ⓝ</div>
-									</div>
-									<div className="flex justify-between">
-										<div className="text-sm">Total bid Amount</div>
-										<div>
-											{watch('bidQuantity', 0) * watch('bidAmount', 0)} Ⓝ
-										</div>
-									</div>
-								</div>
-								<p className="text-gray-900 mt-4 text-sm text-center">
-									You will be redirected to NEAR Web Wallet to confirm your
-									transaction
-								</p>
-								<div className="">
-									<button
-										disabled={isSubmitting}
-										className="w-full outline-none h-12 mt-4 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-2 border-primary bg-primary text-gray-100"
-										type="submit"
-									>
-										{isSubmitting ? 'Redirecting...' : 'Submit Bid'}
-									</button>
-									<button
-										disabled={isSubmitting}
-										className="w-full outline-none h-12 mt-4 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-2 border-primary text-primary"
-										onClick={(_) => {
-											setChosenSeller(null)
-											setShowModal(false)
-										}}
-									>
-										Cancel
-									</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</Modal>
+				<PlaceBidModal
+					localToken={localToken}
+					onSubmitForm={_placebid}
+					onCancel={() => setShowModal('')}
+					isSubmitting={isSubmitting}
+				/>
 			)}
 			{showModal === 'confirmTransfer' && (
 				<Modal
@@ -1892,7 +1817,7 @@ const CardDetail = ({ token }) => {
 
 								{activeTab === 'bids' && (
 									<BidList
-										tokenId={token.tokenId}
+										token={localToken}
 										userOwnership={_getUserOwnership(store.currentUser)}
 									/>
 								)}
