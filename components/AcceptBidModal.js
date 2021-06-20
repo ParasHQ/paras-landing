6199c2d35844263cf18b56fba352f47ba8e22fec
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-06-20 19:18:15 +0700

    accept bid modal

---
+++ components/AcceptBidModal.js
@@ -0,0 +1,74 @@
+import { formatNearAmount } from 'near-api-js/lib/utils/format'
+import { prettyBalance } from '../utils/common'
+import Modal from './Modal'
+
+const AcceptBidModal = ({
+	onClose,
+	token,
+	data,
+	userOwnership,
+	isLoading,
+	onSubmitForm,
+}) => {
+	return (
+		<Modal closeOnBgClick={false} closeOnEscape={false} close={onClose}>
+			<div className="max-w-sm w-full p-4 bg-gray-100 m-auto rounded-md">
+				<div>
+					<h1 className="text-2xl font-bold text-gray-900 tracking-tight">
+						Accept a Bid
+					</h1>
+					<p className="text-gray-900 mt-2">
+						You are about to accept bid for <b>{token.metadata.name}</b> from{' '}
+						<b>ahnaf.near</b>
+					</p>
+					<div className="text-gray-900 mt-4 text-2xl font-bold text-center">
+						{`${prettyBalance(data.bidMarketData.amount, 24, 4)} Ⓝ `}
+						<span className="font-normal text-base">for </span>
+						{`${
+							data.bidMarketData.quantity > userOwnership.quantity
+								? userOwnership.quantity
+								: data.bidMarketData.quantity
+						} pcs`}
+					</div>
+					<div className="mt-4 text-center">
+						<div className="flex justify-between">
+							<div className="text-sm">Service Fee</div>
+							<div>0.5 Ⓝ</div>
+						</div>
+						<div className="flex justify-between">
+							<div className="text-sm">You will get</div>
+							<div>
+								{formatNearAmount(data.bidMarketData.amount) *
+									(data.bidMarketData.quantity > userOwnership.quantity
+										? userOwnership.quantity
+										: data.bidMarketData.quantity)}{' '}
+								Ⓝ
+							</div>
+						</div>
+					</div>
+					<p className="text-gray-900 mt-4 text-sm text-center">
+						Please make sure that your card is not on sale to accept the bid
+					</p>
+					<div className="">
+						<button
+							disabled={isLoading}
+							className="w-full outline-none h-12 mt-4 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-2 border-primary bg-primary text-gray-100"
+							onClick={onSubmitForm}
+						>
+							{isLoading ? 'Accepting...' : 'Accept Bid'}
+						</button>
+						<button
+							disabled={isLoading}
+							className="w-full outline-none h-12 mt-4 rounded-md bg-transparent text-sm font-semibold border-2 px-4 py-2 border-primary text-primary"
+							onClick={onClose}
+						>
+							Cancel
+						</button>
+					</div>
+				</div>
+			</div>
+		</Modal>
+	)
+}
+
+export default AcceptBidModal
