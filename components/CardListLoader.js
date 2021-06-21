201048948aaef00a92972bab1d8ff28c8a6f48e8
hdriqi <riqi.asdf@gmail.com>
2021-06-21 19:09:18 +0700

    add uniqueKey on CardListLoader

--- components/CardListLoader.js
+++ components/CardListLoader.js
@@ -8,7 +8,7 @@ const CardListLoader = () => (
 					key={k}
 					className="w-full md:w-1/3 lg:w-1/4 flex-shrink-0 p-8 relative "
 				>
-					<CardLoader />
+					<CardLoader uniqueKey={k} />
 				</div>
 			)
 		})}
