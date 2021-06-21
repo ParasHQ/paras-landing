abc8cd2a6531274899b8926748eb1fb676bede37
hdriqi <riqi.asdf@gmail.com>
2021-06-21 14:39:07 +0700

    increase z-index for footer

--- components/Footer.js
+++ components/Footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
 	}
 
 	return (
-		<div className="max-w-6xl m-auto px-4 flex flex-wrap justify-center md:justify-between mt-20 text-sm text-gray-400">
+		<div className="max-w-6xl m-auto px-4 flex flex-wrap justify-center md:justify-between mt-20 text-sm text-gray-400 z-10 relative">
 			<div className="py-2">
 				<div className="flex flex-wrap justify-center md:justify-start -mx-2">
 					<div className="flex items-center pt-2 px-2">
