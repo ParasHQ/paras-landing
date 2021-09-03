b1bb4af05671684c9b653a9bf1d92a888c926e0f
hdriqi <riqi.asdf@gmail.com>
2021-09-03 20:44:30 +0700

    add: tab history

--- components/Tabs/TabHistory.js
+++ components/Tabs/TabHistory.js
@@ -1,11 +1,71 @@
+import axios from 'axios'
 import LinkToProfile from 'components/Common/LinkToProfile'
-import { prettyBalance } from 'utils/common'
+import Link from 'next/link'
+import { formatNearAmount } from 'near-api-js/lib/utils/format'
+import { useEffect, useState } from 'react'
+import { timeAgo } from 'utils/common'
 
-const TabHistory = () => {
+const FETCH_TOKENS_LIMIT = 12
+
+const TabHistory = ({ localToken }) => {
+	const [history, setHistory] = useState([])
+	const [page, setPage] = useState(0)
+	const [hasMore, setHasMore] = useState(true)
+	const [isFetching, setIsFetching] = useState(false)
+
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	useEffect(() => {
+		if (localToken.token_series_id) {
+			fetchAllHistory()
+		}
+	}, [localToken])
+
+	const fetchAllHistory = async () => {
+		const _hasMore = await fetchHistory()
+
+		if (_hasMore) {
+			fetchAllHistory()
+		}
+	}
+
+	const fetchHistory = async () => {
+		if (!hasMore || isFetching) {
+			return
+		}
+
+		setIsFetching(true)
+
+		const params = {
+			__skip: page * FETCH_TOKENS_LIMIT,
+			__limit: FETCH_TOKENS_LIMIT,
+		}
+
+		if (localToken.token_id) {
+			params.token_id = localToken.token_id
+		} else {
+			params.token_series_id = localToken.token_series_id
+		}
+
+		const resp = await axios.get(`${process.env.V2_API_URL}/activities`, {
+			params: params,
+		})
+		const newData = resp.data.data
+
+		const newHistory = [...(history || []), ...newData.results]
+		setHistory(newHistory)
+		setPage(page + 1)
+		const _hasMore = newData.results.length < FETCH_TOKENS_LIMIT ? false : true
+
+		setHasMore(_hasMore)
+		setIsFetching(false)
+
+		return _hasMore
+	}
+
 	return (
 		<div className="mt-4 text-white">
-			{[].map((history) => (
-				<Activity key={history._id} activity={history} />
+			{history.map((h) => (
+				<Activity key={h._id} activity={h} />
 			))}
 		</div>
 	)
@@ -13,84 +73,119 @@ const TabHistory = () => {
 
 const Activity = ({ activity }) => {
 	const TextActivity = ({ type }) => {
-		if (type === 'marketUpdate') {
+		if (type === 'add_market_data' || type === 'update_market_data') {
 			return (
 				<p>
-					<LinkToProfile accountId={activity.from} />
+					<LinkToProfile accountId={activity.msg.params.owner_id} />
 					<span>
 						{' '}
-						put on sale for {prettyBalance(activity.amount, 24, 4)} Ⓝ
+						put on sale for {formatNearAmount(activity.msg.params.price)} Ⓝ
 					</span>
 				</p>
 			)
 		}
 
-		if (type === 'marketDelete') {
+		if (type === 'delete_market_data') {
 			return (
 				<p>
-					<LinkToProfile accountId={activity.from} />
+					<LinkToProfile accountId={activity.msg.params.owner_id} />
 					<span> remove from sale</span>
 				</p>
 			)
 		}
 
-		if (type === 'marketBuy') {
+		if (type === 'resolve_purchase') {
 			return (
 				<p>
 					<LinkToProfile accountId={activity.from} />
-					<span> bought {activity.quantity}pcs from </span>
+					<span> bought from </span>
 					<LinkToProfile accountId={activity.to} />
 					<span> for </span>
-					{prettyBalance(activity.amount, 24, 4)} Ⓝ
+					{formatNearAmount(activity.msg.params.price)} Ⓝ
 				</p>
 			)
 		}
 
-		if (type === 'transfer' && activity.from === '') {
+		if (type === 'nft_transfer' && activity.from === null) {
+			const [series_id, edition_id] = activity.msg.params.token_id.split(':')
+
 			return (
 				<p>
 					<LinkToProfile accountId={activity.to} />
-					<span> create with supply of {activity.quantity}pcs</span>
+					<span> minted #{edition_id || 1}</span>
 				</p>
 			)
 		}
 
-		if (type === 'transfer' && activity.to === '') {
+		if (type === 'nft_transfer' && activity.to === null) {
+			const [series_id, edition_id] = activity.msg.params.token_id.split(':')
+
 			return (
 				<p>
 					<LinkToProfile accountId={activity.from} />
-					<span> burned {activity.quantity}pcs</span>
+					<span> burned #{edition_id || 1}</span>
 				</p>
 			)
 		}
 
-		if (type === 'transfer' && !activity.to) {
-			return null
-		}
-
-		if (type === 'bidMarketAdd') {
+		if (type === 'nft_transfer') {
 			return (
 				<p>
-					<LinkToProfile accountId={activity.accountId} />
-					<span> placed offer for </span>
-					<span>{prettyBalance(activity.amount, 24, 4)} Ⓝ</span>
+					<LinkToProfile accountId={activity.to} />
+					<span> transferred to </span>
+					<LinkToProfile accountId={activity.from} />
 				</p>
 			)
 		}
 
-		return (
-			<p>
-				<LinkToProfile accountId={activity.from} />
-				<span> transfer {activity.quantity}pcs to </span>
-				<LinkToProfile accountId={activity.to} />
-			</p>
-		)
+		if (type === 'nft_create_series') {
+			return (
+				<p>
+					<span>Series created by </span>
+					<LinkToProfile accountId={activity.msg.params.creator_id} />
+				</p>
+			)
+		}
+
+		if (type === 'nft_set_series_price') {
+			return (
+				<p>
+					<span>
+						put the series on sale for{' '}
+						{formatNearAmount(activity.msg.params.price)} Ⓝ
+					</span>
+				</p>
+			)
+		}
+
+		if (type === 'nft_set_series_non_mintable') {
+			return (
+				<p>
+					<span> put the series to non-mintable </span>
+				</p>
+			)
+		}
+
+		if (type === 'nft_decrease_series_copies') {
+			return (
+				<p>
+					<span>
+						{' '}
+						decrease the series copies to {activity.msg.params.copies}{' '}
+					</span>
+				</p>
+			)
+		}
+
+		return null
 	}
 
 	return (
 		<div className="bg-gray-900 border border-blueGray-700 mt-3 p-3 rounded-md shadow-md">
-			{TextActivity(activity.type)}
-			<p className="mt-1 text-sm">{activity.createdAt}</p>
+			<TextActivity type={activity.type} />
+			<p className="mt-1 text-sm">
+				{timeAgo.format(new Date(activity.msg.datetime))}
+			</p>
 		</div>
 	)
 }
