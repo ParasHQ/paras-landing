e1f07498fac3a68ba2d8d1eec1a1194b1bb23fac
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-09-05 13:03:00 +0700

    feat: tab info token more info

--- components/TokenInfoCopy.js
+++ components/TokenInfoCopy.js
@@ -16,7 +16,7 @@ const TokenInfoCopy = ({ text, small = false }) => {
 			>
 				<div className="flex">
 					<p
-						className={`whitespace-no-wrap truncate text-gray-800 ${
+						className={`whitespace-no-wrap truncate text-gray-100 ${
 							small ? 'w-32' : 'w-40'
 						}`}
 					>
@@ -34,7 +34,7 @@ const TokenInfoCopy = ({ text, small = false }) => {
 								fillRule="evenodd"
 								clipRule="evenodd"
 								d="M9.70711 14.2929L19 5L20.4142 6.41421L9.70711 17.1213L4 11.4142L5.41421 10L9.70711 14.2929Z"
-								fill="#2d3748"
+								fill="rgba(243, 244, 246)"
 							/>
 						</svg>
 					) : (
@@ -49,7 +49,7 @@ const TokenInfoCopy = ({ text, small = false }) => {
 								fillRule="evenodd"
 								clipRule="evenodd"
 								d="M10 2H20C21.1523 2 22 2.84772 22 4V14C22 15.1523 21.1523 16 20 16H16V20C16 21.1523 15.1523 22 14 22H4C2.84772 22 2 21.1523 2 20V10C2 8.84772 2.84772 8 4 8H8V4C8 2.84772 8.84772 2 10 2ZM8 10H4V20H14V16H10C8.84772 16 8 15.1523 8 14V10ZM10 4V14H20V4H10Z"
-								fill="#2d3748"
+								fill="rgba(243, 244, 246)"
 							/>
 						</svg>
 					)}
