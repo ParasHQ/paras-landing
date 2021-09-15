690fc31820029a16794dbfe991f2dbabcd592e68
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2021-09-15 18:57:41 +0700

    sent sentry only on production

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,16 +2,17 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
-
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://1148bd7ae2364e64bcf607d8bc4cca8a@o989538.ingest.sentry.io/5946256',
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
-});
+	dsn:
+		process.env.NODE_ENV === 'production'
+			? 'https://1148bd7ae2364e64bcf607d8bc4cca8a@o989538.ingest.sentry.io/5946256'
+			: '',
+	// Adjust this value in production, or use tracesSampler for greater control
+	tracesSampleRate: 1.0,
+	// ...
+	// Note: if you want to override the automatic release value, do not set a
+	// `release` value here - use the environment variable `SENTRY_RELEASE`, so
+	// that it will also get attached to your source maps
+})
