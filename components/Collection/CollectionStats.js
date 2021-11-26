357de04c66db3016a5a706541202236400aee4b0
EdoWahdana <wahdanaedo@gmail.com>
2021-11-27 00:04:43 +0700

    fix: use localeLn

--- components/Collection/CollectionStats.js
+++ components/Collection/CollectionStats.js
@@ -1,31 +1,42 @@
 import { prettyBalance } from 'utils/common'
+import { useIntl } from 'hooks/useIntl'
 
 const CollectionStats = ({ stats }) => {
+	const { localeLn } = useIntl()
+
 	return (
 		<>
 			<div className="text-center rounded-l block p-3 sm:p-5">
 				<p className="text-white font-bold truncate text-md sm:text-sm md:text-2xl">
-					{stats.total_cards ? stats.total_cards : '0'}
+					{stats.total_cards || '0'}
 				</p>
-				<p className="text-gray-400 text-xs sm:text-sm md:text-md lg:text-lg">Total Cards</p>
+				<p className="text-gray-400 text-xs sm:text-sm md:text-md lg:text-lg">
+					{localeLn('TotalCard')}
+				</p>
 			</div>
 			<div className="text-center block p-3 sm:p-5">
 				<p className="text-white font-bold truncate text-md sm:text-sm md:text-2xl">
-					{stats.total_owners ? stats.total_owners : '0'}
+					{stats.total_owners || '0'}
 				</p>
-				<p className="text-gray-400 text-xs sm:text-sm md:text-md lg:text-lg">Total Owners</p>
+				<p className="text-gray-400 text-xs sm:text-sm md:text-md lg:text-lg">
+					{localeLn('TotalOwners')}
+				</p>
 			</div>
 			<div className="text-center block p-3 sm:p-5">
 				<p className="text-white font-bold truncate text-md sm:text-sm md:text-2xl">
-					{prettyBalance(stats.floor_price ? stats.floor_price : '---', 24, 4)} Ⓝ
+					{prettyBalance(stats.floor_price || '---', 24, 4)} Ⓝ
 				</p>
-				<p className="text-gray-400 text-xs sm:text-sm md:text-md lg:text-lg">Floored Price</p>
+				<p className="text-gray-400 text-xs sm:text-sm md:text-md lg:text-lg">
+					{localeLn('FloorPrice')}
+				</p>
 			</div>
 			<div className="text-center rounded-r block p-3 sm:p-5">
 				<p className="text-white font-bold truncate text-md sm:text-sm md:text-2xl">
-					{prettyBalance(stats.avg_price ? stats.avg_price : '0', 24, 4)} Ⓝ
+					{prettyBalance(stats.avg_price || '0', 24, 4)} Ⓝ
 				</p>
-				<p className="text-gray-400 text-xs sm:text-sm md:text-md lg:text-lg">Average Price</p>
+				<p className="text-gray-400 text-xs sm:text-sm md:text-md lg:text-lg">
+					{localeLn('AveragePrice')}
+				</p>
 			</div>
 		</>
 	)
