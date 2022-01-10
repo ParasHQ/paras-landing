a1a30a159ebaf1a269ac3cb438a5420f44cf030d
EdoWahdana <wahdanaedo@gmail.com>
2022-01-10 21:23:15 +0700

    fix: img with Media, tokenPadding

--- components/Common/ArtistVerified.js
+++ components/Common/ArtistVerified.js
@@ -9,10 +9,10 @@ const ArtistVerified = ({ token }) => {
 	const [showTooltip, setShowTooltip] = useState(false)
 
 	useEffect(async () => {
-		if (token.metadata.creator_id) {
+		if (token?.metadata.creator_id) {
 			const profileRes = await cachios.get(`${process.env.V2_API_URL}/profiles`, {
 				params: {
-					accountId: token.metadata.creator_id,
+					accountId: token?.metadata.creator_id,
 				},
 				ttl: 600,
 			})
@@ -23,7 +23,7 @@ const ArtistVerified = ({ token }) => {
 	}, [token])
 
 	const getCreatorId = () => {
-		return token.metadata.creator_id || token.contract_id
+		return token?.metadata.creator_id || token?.contract_id
 	}
 
 	return (
@@ -32,7 +32,7 @@ const ArtistVerified = ({ token }) => {
 			<span className="font-semibold">
 				<Link
 					href={
-						token.metadata.creator_id
+						token?.metadata.creator_id
 							? `/${getCreatorId()}/creation`
 							: `/collection/${getCreatorId()}`
 					}
