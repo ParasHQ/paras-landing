4139ab009dc02b10f27d9e43a25d9df4b44af3c1
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2022-01-17 14:53:38 +0700

    refactor: top users, fix: horizontal scroll (#267)
    * Feat: top users loader, refacor top users (#263)
    
    * refactor: top activity (collection, buyer, seller)
    
    * feat: add loader top users
    
    * refactor: top-collections page
    
    * refactor(top-users): change ssr to csr
    
    * fix: input search horizontal scroll on mobile (#266)
    
    Co-authored-by: Iqbal Utomo <73095492+iqbalutomo@users.noreply.github.com>

--- components/Activity/CollectionTransactionDetail.js
+++ components/Activity/CollectionTransactionDetail.js
@@ -1,50 +1,14 @@
 import axios from 'axios'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import Scrollbars from 'react-custom-scrollbars'
-
-import Card from '../Card/Card'
 
 import { parseImgUrl } from 'utils/common'
 import { useIntl } from 'hooks/useIntl'
 import { formatNearAmount } from 'near-api-js/lib/utils/format'
-import cachios from 'cachios'
-import TokenDetailModal from 'components/Token/TokenDetailModal'
+import TopTransactionCard, { renderThumb } from './TopTransactionCard'
 
-const renderThumb = ({ style, ...props }) => {
-	return (
-		<div
-			{...props}
-			style={{
-				...style,
-				cursor: 'pointer',
-				borderRadius: 'inherit',
-				backgroundColor: 'rgba(255, 255, 255, 0.2)',
-			}}
-		/>
-	)
-}
-
-const CollectionTransactionList = ({ data }) => {
-	const [localToken, setLocalToken] = useState(null)
-
-	return (
-		<>
-			<TokenDetailModal tokens={[localToken]} />
-			{data.map((user, idx) => (
-				<UserTransactionDetail
-					data={user}
-					key={user.account_id}
-					idx={idx}
-					setLocalToken={setLocalToken}
-				/>
-			))}
-		</>
-	)
-}
-
-const UserTransactionDetail = ({ data, idx, setLocalToken }) => {
+const CollectionTransactionDetail = ({ data, idx, setLocalToken }) => {
 	const { localeLn } = useIntl()
 
 	const [colDetail, setColDetail] = useState({})
@@ -68,7 +32,7 @@ const UserTransactionDetail = ({ data, idx, setLocalToken }) => {
 							<div className="cursor-pointer w-20 h-20 rounded-full overflow-hidden bg-primary">
 								<img
 									src={parseImgUrl(colDetail?.media, null, {
-										width: `300`,
+										width: `200`,
 									})}
 									className="object-cover"
 								/>
@@ -84,7 +48,7 @@ const UserTransactionDetail = ({ data, idx, setLocalToken }) => {
 							</Link>
 						)}
 						<p className="text-base text-gray-400">
-							Total sales: {formatNearAmount(data.total_sum)} Ⓝ
+							Total sales: {formatNearAmount(data.total_sum, 2)} Ⓝ
 						</p>
 						<p className="text-base text-gray-400">
 							{localeLn('Card')} sold: {data.contract_token_ids.length}
@@ -103,7 +67,7 @@ const UserTransactionDetail = ({ data, idx, setLocalToken }) => {
 				>
 					<div className="flex py-2">
 						{data.contract_token_ids.slice(0, 5).map((contract_token_id, idx) => (
-							<UserTransactionCard
+							<TopTransactionCard
 								key={idx}
 								contract_token_id={contract_token_id}
 								setLocalToken={setLocalToken}
@@ -116,72 +80,4 @@ const UserTransactionDetail = ({ data, idx, setLocalToken }) => {
 	)
 }
 
-const UserTransactionCard = ({ contract_token_id, setLocalToken }) => {
-	const router = useRouter()
-	const [token, setToken] = useState(null)
-
-	const [contractId, tokenId] = contract_token_id.split('::')
-
-	useEffect(() => {
-		fetchData()
-	}, [])
-
-	const fetchData = async () => {
-		const params = {
-			contract_id: contractId,
-			token_id: tokenId,
-		}
-		const resp = await cachios.get(`${process.env.V2_API_URL}/token`, {
-			params: params,
-			ttl: 60,
-		})
-		setToken(resp.data.data.results[0])
-	}
-
-	if (!token) {
-		return null
-	}
-
-	return (
-		<div
-			id={contract_token_id}
-			className="w-1/3 md:w-1/5 px-2 inline-block whitespace-normal overflow-visible flex-shrink-0"
-		>
-			<div className="w-full m-auto" onClick={() => setLocalToken(token)}>
-				<Card
-					imgUrl={parseImgUrl(token.metadata.media, null, {
-						width: `300`,
-						useOriginal: process.env.APP_ENV === 'production' ? false : true,
-						isMediaCdn: token.isMediaCdn,
-					})}
-					onClick={() => {
-						router.push(
-							{
-								pathname: router.pathname,
-								query: {
-									...router.query,
-									tokenId: token.token_id,
-									contractId: token.contract_id,
-								},
-							},
-							`/token/${token.contract_id}::${token.token_series_id}/${token.token_id}`,
-							{
-								shallow: true,
-								scroll: false,
-							}
-						)
-					}}
-					imgBlur={token.metadata.blurhash}
-					token={{
-						title: token.metadata.title,
-						collection: token.metadata.collection || token.contract_id,
-						copies: token.metadata.copies,
-						creatorId: token.metadata.creator_id || token.contract_id,
-					}}
-				/>
-			</div>
-		</div>
-	)
-}
-
-export default CollectionTransactionList
+export default CollectionTransactionDetail
