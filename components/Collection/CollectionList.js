74ed0b882fd98e57725423dfa612c32f9d2f3211
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2022-01-27 11:05:43 +0700

    fix: style

--- components/Collection/CollectionList.js
+++ components/Collection/CollectionList.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import { parseImgUrl } from 'utils/common'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import CollectionListLoader from './CollectionListLoader'
-import ArtistVerified from 'components/Common/ArtistVerified'
 import { generateFromString } from 'generate-avatar'
 
 const CollectionList = ({ data, fetchData, hasMore }) => {
@@ -59,7 +58,7 @@ const CollectionList = ({ data, fetchData, hasMore }) => {
 									</p>
 								</a>
 							</Link>
-							<div className="flex flex-row flex-wrap text-sm text-gray-400">
+							<div className="flex flex-row flex-wrap text-sm text-gray-400 items-center w-full">
 								<span className="mr-1">collection by</span>
 								<Link href={`/${collection.creator_id}`} shallow={true}>
 									<a>
@@ -69,7 +68,28 @@ const CollectionList = ({ data, fetchData, hasMore }) => {
 									</a>
 								</Link>
 								<span className="ml-1">
-									<ArtistVerified collection={collection} />
+									<svg
+										width="16"
+										height="14"
+										viewBox="0 0 18 17"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M17.8095 8.5L15.8343 6.24143L16.1095 3.25429L13.1871 2.59048L11.6571 0L8.90476 1.1819L6.15238 0L4.62238 2.58238L1.7 3.2381L1.97524 6.23333L0 8.5L1.97524 10.7586L1.7 13.7538L4.62238 14.4176L6.15238 17L8.90476 15.81L11.6571 16.9919L13.1871 14.4095L16.1095 13.7457L15.8343 10.7586L17.8095 8.5Z"
+											fill="white"
+										/>
+										<path
+											d="M7.3956 12.1429L5.66675 6.494H7.62684L8.74022 10.9039H9.06951L10.1855 5.66675H12.1429L10.4141 12.1429H7.3956Z"
+											fill="#0816B3"
+										/>
+										<path
+											fillRule="evenodd"
+											clipRule="evenodd"
+											d="M10.1191 5.26196H14.4169L13.6074 6.88101H10.1191V5.26196Z"
+											fill="#0816B3"
+										/>
+									</svg>
 								</span>
 							</div>
 						</div>
