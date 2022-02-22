33dbedc1ce3b2fcb015d2758d60ed25a2ceb985b
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2022-02-22 18:54:54 +0700

    feat: cover profile collection

--- pages/collection/edit/[collection_id].js
+++ pages/collection/edit/[collection_id].js
@@ -4,6 +4,7 @@ import { InputTextarea } from 'components/Common/form'
 import Footer from 'components/Footer'
 import ImgCrop from 'components/ImgCrop'
 import Nav from 'components/Nav'
+import { useIntl } from 'hooks/useIntl'
 import { useToast } from 'hooks/useToast'
 import near from 'lib/near'
 import { sentryCaptureException } from 'lib/sentry'
@@ -14,9 +15,13 @@ import { useEffect, useState } from 'react'
 import { parseImgUrl } from 'utils/common'
 
 const CollectionPageEdit = ({ collectionId }) => {
+	const { localeLn } = useIntl()
 	const [showImgCrop, setShowImgCrop] = useState(false)
-	const [imgFile, setImgFile] = useState({})
+	const [showCoverCrop, setShowCoverCrop] = useState(false)
+	const [imgFile, setImgFile] = useState()
+	const [coverFile, setCoverFile] = useState()
 	const [imgUrl, setImgUrl] = useState('')
+	const [coverUrl, setCoverUrl] = useState('')
 
 	const [collectionName, setCollectionName] = useState('')
 	const [collectionDesc, setCollectionDesc] = useState('')
@@ -38,6 +43,13 @@ const CollectionPageEdit = ({ collectionId }) => {
 		}
 	}
 
+	const _setCover = async (e) => {
+		if (e.target.files[0]) {
+			setCoverFile(e.target.files[0])
+			setShowCoverCrop(true)
+		}
+	}
+
 	const fetchCollection = async () => {
 		const resp = await axios.get(`${process.env.V2_API_URL}/collections`, {
 			params: {
@@ -48,6 +60,7 @@ const CollectionPageEdit = ({ collectionId }) => {
 		setCollectionName(collectionData.collection)
 		setCollectionDesc(collectionData.description)
 		setImgUrl(collectionData.media)
+		setCoverUrl(collectionData.cover)
 	}
 
 	const _submit = async (e) => {
@@ -57,7 +70,10 @@ const CollectionPageEdit = ({ collectionId }) => {
 
 		const formData = new FormData()
 		if (imgFile) {
-			formData.append('file', imgFile)
+			formData.append('files', imgFile, 'logo')
+		}
+		if (coverFile) {
+			formData.append('files', coverFile, 'cover')
 		}
 		formData.append('collection_id', collectionId)
 		formData.append('description', collectionDesc)
@@ -150,27 +166,70 @@ const CollectionPageEdit = ({ collectionId }) => {
 					}}
 				/>
 			)}
+			{showCoverCrop && (
+				<ImgCrop
+					input={coverFile}
+					size={{
+						width: 1024,
+						height: 384,
+					}}
+					left={() => setShowCoverCrop(false)}
+					right={(res) => {
+						setCoverUrl(res.payload.imgUrl)
+						setCoverFile(res.payload.imgFile)
+						setShowCoverCrop(false)
+					}}
+				/>
+			)}
 			<div className="relative max-w-3xl m-auto py-12 px-4">
 				<div className="text-white font-bold text-4xl">Edit collection {collectionName}</div>
-				<div className="text-white mt-4">Logo</div>
-				<div className="relative cursor-pointer w-32 h-32 overflow-hidden rounded-md mt-2">
-					<input
-						className="cursor-pointer w-full opacity-0 absolute inset-0"
-						type="file"
-						accept="image/*"
-						onChange={_setImg}
-						onClick={(e) => {
-							e.target.value = null
-						}}
-					/>
-					<div className="flex items-center justify-center">
-						<div className="w-32 h-32 overflow-hidden bg-primary shadow-inner">
-							<img
-								src={parseImgUrl(imgUrl, null, {
-									width: `300`,
-								})}
-								className="w-full object-cover"
+				<div className="md:flex gap-8">
+					<div>
+						<div className="text-white mt-4">{localeLn('Logo')}</div>
+						<div className="relative cursor-pointer w-32 h-32 overflow-hidden rounded-md mt-2">
+							<input
+								className="cursor-pointer w-full opacity-0 absolute inset-0"
+								type="file"
+								accept="image/*"
+								onChange={_setImg}
+								onClick={(e) => {
+									e.target.value = null
+								}}
 							/>
+							<div className="flex items-center justify-center">
+								<div className="w-32 h-32 overflow-hidden bg-primary shadow-inner">
+									<img
+										src={parseImgUrl(imgUrl, null, {
+											width: `300`,
+										})}
+										className="w-full object-cover"
+									/>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div>
+						<div className="text-white mt-4">{localeLn('Cover')}</div>
+						<div className="relative cursor-pointer w-64 h-32 overflow-hidden rounded-md mt-2">
+							<input
+								className="cursor-pointer w-full opacity-0 absolute inset-0"
+								type="file"
+								accept="image/*"
+								onChange={_setCover}
+								onClick={(e) => {
+									e.target.value = null
+								}}
+							/>
+							<div className="flex items-center justify-center">
+								<div className="w-64 h-32 overflow-hidden bg-primary shadow-inner">
+									<img
+										src={parseImgUrl(coverUrl, null, {
+											width: 300,
+										})}
+										className={`w-full ${coverUrl && 'h-full'} object-cover`}
+									/>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
