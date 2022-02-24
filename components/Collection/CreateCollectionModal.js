1c46b03022bc12f9ebeac8c39c498862c0e9b9b3
Yues Tadrik Hafiyan <31039577+yuesth@users.noreply.github.com>
2022-02-24 15:36:41 +0700

    feat: social media collection (#380)
    * feat: social media collection
    
    * fix: fixing bugs
    
    * fix: improved icon, breakpoints
    
    * fix: added prefix input and minor bugs
    
    Co-authored-by: Ahnaf Alfariza <ahnafalfariza@outlook.com>

--- components/Collection/CreateCollectionModal.js
+++ components/Collection/CreateCollectionModal.js
@@ -1,16 +1,19 @@
 import Modal from 'components/Common/Modal'
 import { IconX } from 'components/Icons'
 import CreateCollection from './CreateCollection'
+import Scrollbars from 'react-custom-scrollbars'
 
 const CreateCollectionModal = ({ onClose, show, onFinishCreate }) => {
 	return (
 		<Modal close={onClose} isShow={show} closeOnEscape={false} closeOnBgClick={false}>
-			<div className="bg-gray-800 m-auto w-full max-w-md rounded-md relative">
-				<CreateCollection onFinishCreate={onFinishCreate} />
-				<div className="absolute top-0 right-0 p-4 cursor-pointer" onClick={onClose}>
-					<IconX />
+			<Scrollbars autoHeight autoHeightMax="90vh">
+				<div className="bg-gray-800 m-auto w-full max-w-md rounded-md relative">
+					<CreateCollection onFinishCreate={onFinishCreate} oneGrid={true} />
+					<div className="absolute top-0 right-0 p-4 cursor-pointer" onClick={onClose}>
+						<IconX />
+					</div>
 				</div>
-			</div>
+			</Scrollbars>
 		</Modal>
 	)
 }
