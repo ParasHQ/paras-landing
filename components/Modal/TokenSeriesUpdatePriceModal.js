538b1b643f0b490c7f07e05e96efaf7aeaf0c23e
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2022-03-06 00:00:41 +0700

    fix: start_time, current_fee, and next_fee

--- components/Modal/TokenSeriesUpdatePriceModal.js
+++ components/Modal/TokenSeriesUpdatePriceModal.js
@@ -93,7 +93,12 @@ const TokenSeriesUpdatePriceModal = ({ show, onClose, data }) => {
 
 	const calculatePriceDistribution = () => {
 		if (newPrice && JSBI.greaterThan(JSBI.BigInt(parseNearAmount(newPrice)), JSBI.BigInt(0))) {
-			const fee = JSBI.BigInt(txFee?.current_fee || 0)
+			let fee
+			if (txFee?.start_time && new Date() < new Date(txFee?.start_time)) {
+				fee = JSBI.BigInt(txFee?.next_fee || 0)
+			} else {
+				fee = JSBI.BigInt(txFee?.current_fee || 0)
+			}
 
 			const calcRoyalty =
 				Object.values(data.royalty).length > 0
