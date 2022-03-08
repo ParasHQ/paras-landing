d1a33b941127de0f780c92f98f9978adb0c394b5
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2022-03-08 13:37:52 +0700

    feat: add loading state on button component

--- components/Common/Button.js
+++ components/Common/Button.js
@@ -9,6 +9,7 @@ const Button = ({
 	children,
 	className,
 	style,
+	isLoading,
 	onClick,
 }) => {
 	const buttonBaseStyle =
@@ -64,7 +65,7 @@ const Button = ({
 
 	return (
 		<button disabled={isDisabled} className={buttonStyle} style={style} onClick={onClick}>
-			{children}
+			{isLoading ? <LoaderIcon /> : children}
 		</button>
 	)
 }
@@ -86,3 +87,28 @@ Button.propTypes = {
 }
 
 export default Button
+
+const LoaderIcon = () => (
+	<div className="flex items-center justify-center">
+		<svg
+			className="animate-spin m h-5 w-5 text-white"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+		>
+			<circle
+				className="opacity-25"
+				cx="12"
+				cy="12"
+				r="10"
+				stroke="currentColor"
+				strokeWidth="4"
+			></circle>
+			<path
+				className="opacity-75"
+				fill="currentColor"
+				d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+			></path>
+		</svg>
+	</div>
+)
