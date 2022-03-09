736f3897fcc7363adf6cafe7414e8c954e0e2d9f
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2022-03-09 17:37:17 +0700

    refactor: notification wording

--- components/Notification/NotificationItem.js
+++ components/Notification/NotificationItem.js
@@ -94,7 +94,11 @@ const NotificationItem = ({ notif, currentUser, notificationModal }) => {
 						>
 							<NotificationImage media={token.metadata?.media} />
 							<div className="pl-2 text-gray-300">
-								Creator minted #{token.edition_id || token.token_id} to {notif.to}
+								{`Creator minted #${token.edition_id || token.token_id} to ${prettyTruncate(
+									notif.to,
+									14,
+									'address'
+								)}`}
 							</div>
 						</div>
 					</a>
@@ -113,7 +117,9 @@ const NotificationItem = ({ notif, currentUser, notificationModal }) => {
 							onClick={() => notificationModal(false)}
 						>
 							<NotificationImage media={token.metadata?.media} />
-							<div className="pl-2 text-gray-300">burned #{token.edition_id || token.token_id}</div>
+							<div className="pl-2 text-gray-300">
+								burned <span className="font-medium text-gray-100">{token.metadata?.title}</span>
+							</div>
 						</div>
 					</a>
 				</Link>
@@ -135,8 +141,10 @@ const NotificationItem = ({ notif, currentUser, notificationModal }) => {
 								<div className="pl-2 text-gray-300">
 									sold <span className="font-medium text-gray-100">{token.metadata?.title}</span>
 									{' to '}
-									<span className="font-semibold">{notif.to}</span> for{' '}
-									{formatNearAmount(notif.msg.params.price)} Ⓝ
+									<span className="font-semibold">
+										{prettyTruncate(notif.to, 14, 'address')}
+									</span>{' '}
+									for {formatNearAmount(notif.msg.params.price)} Ⓝ
 								</div>
 							</div>
 						</a>
@@ -160,7 +168,7 @@ const NotificationItem = ({ notif, currentUser, notificationModal }) => {
 									<span className="font-medium text-gray-100">{token.metadata?.title} </span>
 									from{' '}
 									<span className="font-medium text-gray-100">
-										{prettyTruncate(notif.from, 12, 'address')}
+										{prettyTruncate(notif.from, 14, 'address')}
 									</span>
 								</div>
 							</div>
@@ -181,12 +189,25 @@ const NotificationItem = ({ notif, currentUser, notificationModal }) => {
 							onClick={() => notificationModal(false)}
 						>
 							<NotificationImage media={token.metadata?.media} />
-							<div className="pl-2 text-gray-200">
-								sold <span className="font-medium text-gray-100">{token.metadata?.title}</span>
-								{' to '}
-								<span className="font-semibold">{notif.to}</span> for{' '}
-								{formatNearAmount(notif.msg.params.price)} Ⓝ
-							</div>
+							{notif.to === currentUser ? (
+								<div className="pl-2 text-gray-200">
+									bought <span className="font-medium text-gray-100">{token.metadata?.title}</span>
+									{' from '}
+									<span className="font-semibold">
+										{prettyTruncate(notif.from, 14, 'address')}
+									</span>{' '}
+									for {formatNearAmount(notif.msg.params.price)} Ⓝ
+								</div>
+							) : (
+								<div className="pl-2 text-gray-200">
+									sold <span className="font-medium text-gray-100">{token.metadata?.title}</span>
+									{' to '}
+									<span className="font-semibold">
+										{prettyTruncate(notif.to, 14, 'address')}
+									</span>{' '}
+									for {formatNearAmount(notif.msg.params.price)} Ⓝ
+								</div>
+							)}
 						</div>
 					</a>
 				</Link>
@@ -227,8 +248,8 @@ const NotificationItem = ({ notif, currentUser, notificationModal }) => {
 						>
 							<NotificationImage media={token.metadata?.media} />
 							<div className="pl-2 text-gray-100">
-								<span className="font-semibold">{notif.from}</span> offer{' '}
-								<span className="font-medium text-gray-100">{token.metadata?.title}</span>
+								<span className="font-semibold">{prettyTruncate(notif.from, 14, 'address')}</span>{' '}
+								offer <span className="font-medium text-gray-100">{token.metadata?.title}</span>
 								{' for '}
 								{formatNearAmount(notif.msg.params.price)} Ⓝ
 							</div>
