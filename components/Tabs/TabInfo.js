2b55204f9fbdfb9e5140121eee4a342f0d2a13a0
Iqbal Utomo <73095492+iqbalutomo@users.noreply.github.com>
2022-03-24 15:06:10 +0700

    feat: view count on token (#483)

--- components/Tabs/TabInfo.js
+++ components/Tabs/TabInfo.js
@@ -209,38 +209,36 @@ const TabInfo = ({ localToken, isNFT }) => {
 						</div>
 					</div>
 				)}
-				{!isNFT && (
-					<div className="flex flex-1 bg-gray-800 mt-3 p-3 rounded-md shadow-md">
-						<div>
-							<p className="text-sm text-white font-bold">{localeLn('Views')}</p>
-							<div className="text-gray-100 font-semibold">
-								<div className="flex gap-1 items-start">
-									<svg
-										className="w-5 h-5"
-										fill="none"
-										stroke="currentColor"
-										viewBox="0 0 24 24"
-										xmlns="http://www.w3.org/2000/svg"
-									>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth="2"
-											d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-										></path>
-										<path
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											strokeWidth="2"
-											d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-										></path>
-									</svg>
-									{localToken.view}
-								</div>
+				<div className="flex flex-1 bg-gray-800 mt-3 p-3 rounded-md shadow-md">
+					<div>
+						<p className="text-sm text-white font-bold">{localeLn('Views')}</p>
+						<div className="text-gray-100 font-semibold">
+							<div className="flex gap-1 items-start">
+								<svg
+									className="w-5 h-5"
+									fill="none"
+									stroke="currentColor"
+									viewBox="0 0 24 24"
+									xmlns="http://www.w3.org/2000/svg"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+									></path>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+									></path>
+								</svg>
+								{!localToken.view ? '0' : localToken.view}
 							</div>
 						</div>
 					</div>
-				)}
+				</div>
 			</div>
 			{localToken.metadata.attributes && (
 				<div className="flex bg-gray-800 mt-3 p-3 rounded-md shadow-md">
