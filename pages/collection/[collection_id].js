a5e3b713a9a312329b78227bb417dd1be0a8ef68
yuesth <yues.963@gmail.com>
2022-04-04 14:51:40 +0700

    feat: changed icon and added in colllection name and activity

--- pages/collection/[collection_id].js
+++ pages/collection/[collection_id].js
@@ -436,9 +436,38 @@ const CollectionPage = ({ collectionId, collection, serverQuery }) => {
 						/>
 					</div>
 				</div>
-				<h1 className="text-4xl font-bold text-gray-100 mx-4 text-center break-words">
-					{collection?.collection}
-				</h1>
+				<div className="flex items-center justify-center">
+					<h1 className="text-4xl font-bold text-gray-100 text-center break-words">
+						{collection?.collection}
+					</h1>
+					{tokens[0]?.contract_id && (
+						<a
+							href={`https:///explorer.${
+								process.env.APP_ENV === 'production' ? `mainnet` : `testnet`
+							}.near.org/accounts/${tokens[0]?.contract_id}`}
+							target={`_blank`}
+							className="ml-1 mb-4"
+						>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								className="icon icon-tabler icon-tabler-external-link"
+								width={20}
+								height={20}
+								viewBox="0 0 24 24"
+								strokeWidth="2"
+								stroke="#fff"
+								fill="none"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+							>
+								<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+								<path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+								<line x1={10} y1={14} x2={20} y2={4} />
+								<polyline points="15 4 20 4 20 9" />
+							</svg>
+						</a>
+					)}
+				</div>
 				<div className="m-4 mt-0 text-center relative">
 					<h4 className="text-xl md:flex justify-center text-gray-300 self-center break-words">
 						<span>collection by</span>
