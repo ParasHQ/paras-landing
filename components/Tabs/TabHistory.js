b5ae490e9795de15a3c3f017f64af50e283a109a
yuesth <yues.963@gmail.com>
2022-04-05 11:43:19 +0700

    fix:changed icon

--- components/Tabs/TabHistory.js
+++ components/Tabs/TabHistory.js
@@ -426,7 +426,7 @@ const TxHashLink = ({ activity }) => {
 					<div className="w-8 h-8 rounded-full transition-all duration-200 hover:bg-dark-primary-3 flex items-center justify-center">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
-							className="icon icon-tabler icon-tabler-receipt"
+							className="icon icon-tabler icon-tabler-external-link"
 							width={18}
 							height={18}
 							viewBox="0 0 24 24"
@@ -437,7 +437,9 @@ const TxHashLink = ({ activity }) => {
 							strokeLinejoin="round"
 						>
 							<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-							<path d="M5 21v-16a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v16l-3 -2l-2 2l-2 -2l-2 2l-2 -2l-3 2m4 -14h6m-6 4h6m-2 4h2" />
+							<path d="M11 7h-5a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-5" />
+							<line x1={10} y1={14} x2={20} y2={4} />
+							<polyline points="15 4 20 4 20 9" />
 						</svg>
 					</div>
 				</a>
