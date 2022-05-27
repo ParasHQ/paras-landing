47b7ca81b96031331d9c9fed922bba34e4aa4aeb
Nikita Kuznetsov <n@8gen.team>
2022-05-27 20:03:31 +0200

    chore: handle exception on CID parser

--- utils/common.js
+++ utils/common.js
@@ -110,7 +110,13 @@ export const parseImgUrl = (imgUrl, defaultValue = '', opts = {}) => {
 		let parts = url.split('/')
 		let hash = parts[2]
 		let path = parts.length > 3 ? `/${parts.slice(3).join('/')}` : ''
-		let cid = new CID(hash)
+		let cid
+		try {
+			cid = new CID(hash)
+		} catch (e) {
+			console.error(`Unable to parse CID: ${hash}`, e)
+			return imgUrl
+		}
 
 		if (opts.useOriginal || process.env.APP_ENV !== 'production') {
 			if (cid.version === 0) {
