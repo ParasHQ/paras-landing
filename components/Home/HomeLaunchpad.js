5bb25ceb777c443d9f67db484cb988e6f217baba
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2022-06-22 14:31:11 +0700

    feat: project detail & refactoring icon discord

--- components/Home/HomeLaunchpad.js
+++ components/Home/HomeLaunchpad.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 import useSWR from 'swr'
 import { useRef, useState } from 'react'
-import CollectionListLoader from 'components/Collection/CollectionListLoader'
 import { IconLeft, IconRight } from 'components/Icons'
 import { LaunchpadItem } from 'components/Launchpad/LaunchpadList'
+import ProjectListLoader from 'components/Collection/ProjectListLoader'
 
 const HomeLaunchpad = ({ showDetails }) => {
 	const [showLeftClick, setShowLeftClick] = useState(false)
@@ -51,29 +51,13 @@ const HomeLaunchpad = ({ showDetails }) => {
 		setIsActive(type)
 	}
 
-	if (!data && isValidating) {
-		return (
-			<div className="my-8 relative">
-				<h1 className="text-white font-semibold text-3xl capitalize mb-4">Upcoming Drops</h1>
-				<div className="md:-mx-3">
-					<div className="hidden md:block">
-						<CollectionListLoader uniqueKey="big-collection-loader" contentLength={3} />
-					</div>
-					<div className="md:hidden">
-						<CollectionListLoader uniqueKey="small-collection-loader" contentLength={1} />
-					</div>
-				</div>
-			</div>
-		)
-	}
-
 	return (
-		<div className="mb-8 relative">
-			<div className="text-white md:flex justify-start items-center gap-4 mb-4">
+		<div className="md:mb-8 relative">
+			<div className="text-white md:flex justify-start items-center gap-4 mb-8">
 				<h1 className="font-semibold text-3xl capitalize">Upcoming Drops</h1>
-				<div className="flex justify-around items-center mx-[67px] md:mx-0 border-2 border-gray-800 rounded-full text-sm font-thin mt-2 md:mt-0">
+				<div className="flex justify-between items-center mx-10 md:mx-0 border-2 border-gray-800 rounded-full text-sm font-thin mt-2 md:mt-0">
 					<div
-						className={`pr-2 pl-2 py-3 rounded-l-full ${
+						className={`pr-2 pl-6 py-2 rounded-l-full ${
 							isActive === 'next-days' && 'bg-primary'
 						} cursor-pointer`}
 						onClick={() => tabAction('next-days')}
@@ -81,13 +65,13 @@ const HomeLaunchpad = ({ showDetails }) => {
 						Next 7 days
 					</div>
 					<div
-						className={`px-2 py-3 ${isActive === 'coming-soon' && 'bg-primary'} cursor-pointer`}
+						className={`px-2 py-2 ${isActive === 'coming-soon' && 'bg-primary'} cursor-pointer`}
 						onClick={() => tabAction('coming-soon')}
 					>
 						Coming Soon
 					</div>
 					<div
-						className={`pr-6 pl-2 py-3 ${
+						className={`pr-6 pl-2 py-2 ${
 							isActive === 'live' && 'bg-primary'
 						} rounded-r-full cursor-pointer`}
 						onClick={() => tabAction('live')}
@@ -96,38 +80,51 @@ const HomeLaunchpad = ({ showDetails }) => {
 					</div>
 				</div>
 			</div>
-			<div
-				ref={ref}
-				className="flex flex-nowrap overflow-scroll -mx-3 disable-scrollbars"
-				onScroll={onScroll}
-			>
-				{data.map((collection) => (
-					<LaunchpadItem
-						key={collection._id}
-						collection={collection}
-						showDetails={showDetails}
-						className="rounded-md overflow-hidden snap-center w-1/2 md:w-1/4 px-3 shrink-0"
-					/>
-				))}
-			</div>
-			<div className="flex absolute right-0 top-0 gap-1 mt-2">
-				<div
-					className={`${
-						showLeftClick ? 'text-gray-200 cursor-pointer' : 'text-gray-500 cursor-not-allowed'
-					}`}
-					onClick={scrollToLeft}
-				>
-					<IconLeft />
+			{!data && isValidating ? (
+				<div className="-mx-3">
+					<div className="hidden md:block">
+						<ProjectListLoader uniqueKey="big-project-loader" contentLength={4} />
+					</div>
+					<div className="md:hidden">
+						<ProjectListLoader uniqueKey="small-project-loader" contentLength={2} />
+					</div>
 				</div>
-				<div
-					className={`${
-						showRightClick ? 'text-gray-200 cursor-pointer' : 'text-gray-500 cursor-not-allowed'
-					}`}
-					onClick={scrollToRight}
-				>
-					<IconRight />
-				</div>
-			</div>
+			) : (
+				<>
+					<div
+						ref={ref}
+						className="flex flex-nowrap overflow-scroll -mx-3 disable-scrollbars"
+						onScroll={onScroll}
+					>
+						{data?.map((project) => (
+							<LaunchpadItem
+								key={project._id}
+								project={project}
+								showDetails={showDetails}
+								className="rounded-md overflow-hidden snap-center w-1/2 md:w-1/4 px-3 shrink-0"
+							/>
+						))}
+					</div>
+					<div className="flex absolute right-0 top-0 gap-1 mt-2">
+						<div
+							className={`${
+								showLeftClick ? 'text-gray-200 cursor-pointer' : 'text-gray-500 cursor-not-allowed'
+							}`}
+							onClick={scrollToLeft}
+						>
+							<IconLeft />
+						</div>
+						<div
+							className={`${
+								showRightClick ? 'text-gray-200 cursor-pointer' : 'text-gray-500 cursor-not-allowed'
+							}`}
+							onClick={scrollToRight}
+						>
+							<IconRight />
+						</div>
+					</div>
+				</>
+			)}
 		</div>
 	)
 }
--- components/Launchpad/LaunchpadList.js
+++ components/Launchpad/LaunchpadList.js
@@ -1,11 +1,8 @@
-import Link from 'next/link'
 import { parseImgUrl } from 'utils/common'
 import InfiniteScroll from 'react-infinite-scroll-component'
-import CollectionListLoader from 'components/Collection/CollectionListLoader'
+import ProjectListLoader from 'components/Collection/ProjectListLoader'
 import { generateFromString } from 'generate-avatar'
-import { formatNearAmount } from 'near-api-js/lib/utils/format'
 import router from 'next/router'
-import { trackCollectionList } from 'lib/ga'
 
 const LaunchpadList = ({ data, fetchData, hasMore, page }) => {
 	if (data.length === 0 && !hasMore) {
@@ -15,7 +12,7 @@ const LaunchpadList = ({ data, fetchData, hasMore, page }) => {
 					<div className="w-40 m-auto">
 						<img src="/cardstack.png" className="opacity-75" />
 					</div>
-					<p className="mt-4">No Collections</p>
+					<p className="mt-4">No Project</p>
 				</div>
 			</div>
 		)
@@ -23,16 +20,16 @@ const LaunchpadList = ({ data, fetchData, hasMore, page }) => {
 
 	return (
 		<InfiniteScroll
-			dataLength={data?.length || 3}
+			dataLength={data?.length || 4}
 			next={fetchData}
 			hasMore={hasMore}
-			loader={<CollectionListLoader />}
+			loader={<ProjectListLoader />}
 		>
 			<div className="flex flex-wrap pt-4">
-				{data.map((collection, index) => (
+				{data.map((project, index) => (
 					<LaunchpadItem
 						key={index}
-						collection={collection}
+						project={project}
 						fullOpacity={page !== 'search'}
 						className="rounded-md overflow-hidden mb-12 md:mb-8 w-full md:w-1/3 md:px-4"
 					/>
@@ -44,30 +41,20 @@ const LaunchpadList = ({ data, fetchData, hasMore, page }) => {
 
 export default LaunchpadList
 
-export const LaunchpadItem = ({
-	collection,
-	fullOpacity = true,
-	className,
-	showDetails = true,
-}) => {
-	const defaultAvatar = `data:image/svg+xml;utf8,${generateFromString(collection.collection_id)}`
+export const LaunchpadItem = ({ project, fullOpacity = true, className, showDetails = true }) => {
+	const defaultAvatar = `data:image/svg+xml;utf8,${generateFromString(project.collection_id)}`
 
-	const onToCollection = () => {
-		if (router.pathname === '/') {
-			trackCollectionList(collection.collection_id)
-		}
-		router.push(`/collection/${collection.collection_id}`, undefined, { shallow: true })
+	const onToProject = () => {
+		router.push(`/launchpad/project-detail/${project.collection_id}`, undefined, { shallow: true })
 	}
 
 	return (
 		<div
-			className={`${className} ${
-				collection?.isCreator || fullOpacity ? 'opacity-100' : 'opacity-60'
-			}`}
+			className={`${className} ${project?.isCreator || fullOpacity ? 'opacity-100' : 'opacity-60'}`}
 		>
-			<div onClick={onToCollection}>
+			<div onClick={onToProject}>
 				<a
-					href={`/collection/${collection.collection_id}`}
+					href={`/launchpad/project-detail/${project.collection_id}`}
 					onClick={(e) => e.preventDefault()}
 					className="cursor-pointer"
 				>
@@ -75,7 +62,7 @@ export const LaunchpadItem = ({
 						<div className="w-full h-full mb-4 rounded">
 							<img
 								className="object-cover w-full md:h-40 h-full p-1 transform ease-in-out duration-200 hover:opacity-80 rounded-xl"
-								src={parseImgUrl(collection.cover || collection.media || defaultAvatar, {
+								src={parseImgUrl(project.cover || project.media || defaultAvatar, {
 									width: '200',
 								})}
 							/>
@@ -86,14 +73,14 @@ export const LaunchpadItem = ({
 			<div className="text-white mt-1">
 				<div className="flex justify-center">
 					<div>
-						<div onClick={onToCollection}>
+						<div onClick={onToProject}>
 							<a
-								href={`/collection/${collection.collection_id}`}
+								href={`/launchpad/project-detail/${project.collection_id}`}
 								onClick={(e) => e.preventDefault()}
 								className="cursor-pointer"
 							>
 								<p className="grid grid-flow-col text-md hover:underline font-bold">
-									{collection.collection}
+									{project.collection}
 								</p>
 							</a>
 						</div>
@@ -104,7 +91,7 @@ export const LaunchpadItem = ({
 						<div className="flex justify-around my-2">
 							<div className="flex my-1 text-center font-light gap-1">
 								<p className="font-light text-xs text-white text-opacity-70 mb-1">Items</p>
-								<p className="font-bold text-xs">{collection.total_cards || 5555}</p>
+								<p className="font-bold text-xs">{project.total_cards || 5555}</p>
 							</div>
 							<div className="flex my-1 text-center gap-1">
 								<p className="font-light text-xs text-white text-opacity-70 mb-1">Price</p>
