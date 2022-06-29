2129a87379d35e311dbb4221821d8de96db7566f
Alan Maulana <8049855-cryingraven@users.noreply.gitlab.com>
2022-06-29 20:37:27 +0700

    feat: add notif item

--- components/Notification/NotificationItem.js
+++ components/Notification/NotificationItem.js
@@ -449,7 +449,23 @@ const NotificationItem = ({ notif, currentUser, notificationModal }) => {
 			</div>
 		)
 	}
-
+	if (notif.type === 'notification_nft_sold_for_bidder') {
+		return (
+			<div>
+				<Link href={url}>
+					<a>
+						<div
+							className="cursor-pointer p-2 rounded-md button-wrapper flex items-center"
+							onClick={() => notificationModal(false)}
+						>
+							<NotificationImage media={token.metadata?.media} />
+							<div className="pl-2 text-gray-300">{`item sold by owner`}</div>
+						</div>
+					</a>
+				</Link>
+			</div>
+		)
+	}
 	return null
 }
 
