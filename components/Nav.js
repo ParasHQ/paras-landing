ef438162d108a1f5cdccecaa59e00c28cbf3a585
Edo Wahdana <49039269+EdoWahdana@users.noreply.github.com>
2022-07-15 14:18:50 +0700

    fix: fetch small banner (#776)
    * fix: fetch small banner
    
    * fix: small banner if empty

--- components/Nav.js
+++ components/Nav.js
@@ -189,21 +189,13 @@ const Nav = () => {
 				)}
 
 				{/* Banner for special event */}
-				<div
-					className={`relative text-white text-center overflow-hidden text-md md:leading-8 m-auto bg-primary z-50 flex items-center justify-center transition-height duration-500 md:h-8`}
-				>
-					<div className="px-10 py-1 md:p-0 ">
-						{`Congratulations to all `}
-						<a
-							href="https://twitter.com/parashq/status/1544335392174112768?s=21&t=HkTljoa4kFLASLqnhZdjwg"
-							target="_blank"
-							className="font-bold cursor-pointer hover:underline"
-							rel="noreferrer"
-						>
-							{`the winners of June's Card4Card!`}
-						</a>
+				{store.smallBanner && store.smallBanner.is_active && (
+					<div
+						className={`relative text-white text-center overflow-hidden text-md md:leading-8 m-auto bg-primary z-50 flex items-center justify-center transition-height duration-500 md:h-8`}
+					>
+						<div dangerouslySetInnerHTML={{ __html: store.smallBanner.bannerText }}></div>
 					</div>
-				</div>
+				)}
 
 				{process.env.APP_ENV !== 'production' && (
 					<div
