0d193c9a409fcabd5132ba3defc7184625b0a09e
Yues Tadrik Hafiyan <31039577+yuesth@users.noreply.github.com>
2022-07-27 17:33:19 +0700

    fix: ended soonest sort pagination (#836)

--- pages/market.js
+++ pages/market.js
@@ -25,6 +25,7 @@ const MarketPage = ({ serverQuery }) => {
 	const [idNext, setIdNext] = useState(null)
 	const [lowestPriceNext, setLowestPriceNext] = useState(null)
 	const [updatedAtNext, setUpdatedAtNext] = useState(null)
+	const [endedSoonestNext, setEndedSoonestNext] = useState(null)
 	const [isFetching, setIsFetching] = useState(false)
 	const [isFiltering, setIsFiltering] = useState(true)
 	const [display, setDisplay] = useState(
@@ -62,6 +63,9 @@ const MarketPage = ({ serverQuery }) => {
 
 	const updateFilter = async (query) => {
 		setIsFiltering(true)
+		// if (!query?.__sort?.includes('ended_at')) {
+		// 	setEndedSoonestNext(null)
+		// }
 		const params = tokensParams({
 			...(query || serverQuery),
 			liked_by: currentUser,
@@ -79,6 +83,8 @@ const MarketPage = ({ serverQuery }) => {
 			setIdNext(lastData._id)
 			params.__sort.includes('updated_at') && setUpdatedAtNext(lastData.updated_at)
 			params.__sort.includes('lowest_price') && setLowestPriceNext(lastData.lowest_price)
+			params.__sort.includes('ended_at') &&
+				setEndedSoonestNext(lastData.sorted_auction_token?.ended_at)
 		}
 		setIsFiltering(false)
 	}
@@ -105,6 +111,7 @@ const MarketPage = ({ serverQuery }) => {
 						lowest_price_next: lowestPriceNext,
 						updated_at_next: updatedAtNext,
 						liked_by: currentUser,
+						ended_soonest_next: endedSoonestNext,
 				  }),
 		})
 		const res = await axios(`${process.env.V2_API_URL}/token-series`, {
@@ -122,6 +129,8 @@ const MarketPage = ({ serverQuery }) => {
 			setIdNext(lastData._id)
 			params.__sort.includes('updated_at') && setUpdatedAtNext(lastData.updated_at)
 			params.__sort.includes('lowest_price') && setLowestPriceNext(lastData.lowest_price)
+			params.__sort.includes('ended_at') &&
+				setEndedSoonestNext(lastData.sorted_auction_token?.ended_at)
 		}
 		setIsFetching(false)
 	}
@@ -228,6 +237,7 @@ const tokensParams = (query) => {
 		...(query.pmax && { max_price: parseNearAmount(query.pmax) }),
 		...(query.card_trade_type === 'notForSale' && { has_price: false }),
 		...(query._id_next && { _id_next: query._id_next }),
+		...(query.ended_soonest_next && { ended_soonest_next: query.ended_soonest_next }),
 		...(query.lowest_price_next &&
 			parsedSortQuery.includes('lowest_price') && { lowest_price_next: query.lowest_price_next }),
 		...(query.updated_at_next &&
