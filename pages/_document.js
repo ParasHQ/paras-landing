1056152574bcc6feb50f0d9263385d333a921eba
Ahnaf Alfariza <ahnafalfariza@outlook.com>
2022-09-13 19:31:45 +0700

    fix: head in document move to app (#927)

--- pages/_document.js
+++ pages/_document.js
@@ -16,12 +16,6 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 					<Fragment>
-						{process.env.APP_ENV !== 'production' && (
-							<>
-								<meta name="robots" content="noindex" />
-								<meta name="googlebot" content="noindex" />
-							</>
-						)}
 						{/* Global Site Tag (gtag.js) - Google Analytics */}
 						<script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
 						<script
