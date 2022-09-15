a9f23bdbda50a3542742af9b29a322cabae77b73
Edo Wahdana <49039269+EdoWahdana@users.noreply.github.com>
2022-09-15 11:16:02 +0700

    fix: collection name null on share button (#925)
    * fix: collection name null on share button
    
    * fix: use state and loading while fetching

--- components/Modal/ListModal.js
+++ components/Modal/ListModal.js
@@ -3,6 +3,7 @@ import Modal from 'components/Common/Modal'
 const ListModal = ({
 	show,
 	onClose,
+	isFetching = false,
 	list = [
 		{
 			name: 'Copy Link',
@@ -21,11 +22,15 @@ const ListModal = ({
 	return (
 		<Modal isShow={show} close={onClose} closeOnBgClick closeOnEscape>
 			<div className="px-4 py-2 bg-gray-800 max-w-xs w-full rounded-md m-auto">
-				{list.map((item, index) => (
-					<div key={index} className="py-2 text-white cursor-pointer" onClick={item.onClick}>
-						{item.name}
-					</div>
-				))}
+				{isFetching ? (
+					<div className="text-white py-3">Fetching collection data...</div>
+				) : (
+					list.map((item, index) => (
+						<div key={index} className="py-2 text-white cursor-pointer" onClick={item.onClick}>
+							{item.name}
+						</div>
+					))
+				)}
 			</div>
 		</Modal>
 	)
