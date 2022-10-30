ca591dfbe9f2a76572ee3b7877e8950366caf837
EdoWahdana <wahdanaedo@gmail.com>
2022-10-30 20:58:15 +0700

    feat: floating banner

--- components/Banner/FloatingBanner.js
+++ components/Banner/FloatingBanner.js
@@ -1,39 +1,23 @@
 import { useEffect, useState } from 'react'
 import { parseImgUrl } from 'utils/common'
-import LoginModal from 'components/Modal/LoginModal'
+import Link from 'next/link'
 import ParasRequest from 'lib/ParasRequest'
 import useSWR from 'swr'
-import useStore from 'lib/store'
 
 const FloatingBanner = () => {
 	const fetchFloatingBanner = () =>
 		ParasRequest.get(`${process.env.V2_API_URL}/floating-banner`).then((res) => res.data.result[0])
 
-	const currentUser = useStore((state) => state.currentUser)
 	const { data, isValidating } = useSWR('floating-banner', fetchFloatingBanner)
 	const [showFloating, setShowFloating] = useState(false)
 
 	useEffect(() => {
-		if (currentUser && data.is_active) {
-			if (typeof window !== undefined) {
-				const modalLocalStorage = window.localStorage.getItem(data?.event_name)
-
-				if (modalLocalStorage) {
-					setShowFloating(false)
-				} else {
-					setShowFloating(true)
-				}
-			}
-		} else {
+		if (data?.is_active) {
 			setShowFloating(true)
+		} else {
+			setShowFloating(false)
 		}
-	}, [currentUser, data])
-
-	const onClickBanner = async (e) => {
-		e.stopPropagation()
-		setShowFloating(false)
-		localStorage.setItem(data.event_name, true)
-	}
+	}, [data])
 
 	if ((!data && isValidating) || data.length === 0) {
 		return null
@@ -42,16 +26,15 @@ const FloatingBanner = () => {
 	return (
 		<>
 			{showFloating && (
-				<div
-					className="h-32 w-20 p-4 md:m-auto z-20 fixed right-36 bottom-10 left-1/2 transform -translate-x-1/2 md:left-auto md:transform-none cursor-pointer"
-					onClick={onClickBanner}
-				>
-					<div className="absolute right-0">
-						<div className="cursor-pointer">
-							<img src={parseImgUrl(data.image)} />
+				<Link href={`${data?.open_link}`}>
+					<div className="h-36 w-24 p-4 md:m-auto z-20 fixed right-36 bottom-5 left-1/2 transform -translate-x-1/2 md:left-auto md:transform-none cursor-pointer">
+						<div className="absolute right-0">
+							<div className="cursor-pointer">
+								<img src={parseImgUrl(data.image)} />
+							</div>
 						</div>
 					</div>
-				</div>
+				</Link>
 			)}
 		</>
 	)
