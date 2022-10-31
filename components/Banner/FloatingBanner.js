2959cf2868faefc119e2971cf11ccb58578cae09
EdoWahdana <wahdanaedo@gmail.com>
2022-10-31 15:19:51 +0700

    fix: review

--- components/Banner/FloatingBanner.js
+++ components/Banner/FloatingBanner.js
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import { parseImgUrl } from 'utils/common'
 import Link from 'next/link'
 import ParasRequest from 'lib/ParasRequest'
@@ -9,15 +8,6 @@ const FloatingBanner = () => {
 		ParasRequest.get(`${process.env.V2_API_URL}/floating-banner`).then((res) => res.data.result[0])
 
 	const { data, isValidating } = useSWR('floating-banner', fetchFloatingBanner)
-	const [showFloating, setShowFloating] = useState(false)
-
-	useEffect(() => {
-		if (data?.is_active) {
-			setShowFloating(true)
-		} else {
-			setShowFloating(false)
-		}
-	}, [data])
 
 	if ((!data && isValidating) || data.length === 0) {
 		return null
@@ -25,9 +15,9 @@ const FloatingBanner = () => {
 
 	return (
 		<>
-			{showFloating && (
+			{data.is_active && (
 				<Link href={`${data?.open_link}`}>
-					<div className="h-36 w-24 p-4 md:m-auto z-20 fixed right-36 bottom-5 left-1/2 transform -translate-x-1/2 md:left-auto md:transform-none cursor-pointer">
+					<div className="h-36 w-24 p-4 md:m-auto z-20 fixed -right-8 md:right-36 bottom-5 transform -translate-x-1/2 md:left-auto md:transform-none cursor-pointer">
 						<div className="absolute right-0">
 							<div className="cursor-pointer">
 								<img src={parseImgUrl(data.image)} />
