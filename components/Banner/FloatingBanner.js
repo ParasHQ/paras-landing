5728e78de119c8b50261e9d10c4ec9901367de8a
EdoWahdana <wahdanaedo@gmail.com>
2022-11-03 17:35:28 +0700

    feat: scroll debounce and offset

--- components/Banner/FloatingBanner.js
+++ components/Banner/FloatingBanner.js
@@ -12,19 +12,69 @@ const FloatingBanner = () => {
 	const { data, isValidating } = useSWR('floating-banner', fetchFloatingBanner)
 
 	useEffect(() => {
-		window.addEventListener('scroll', (ev) => {
-			if (ev.isTrusted) {
-				bannerRef.current.style.setProperty('transform', 'translate(70px)')
-				bannerRef.current.style.setProperty('transition-duration', '.3s')
-				bannerRef.current.style.setProperty('transition-timing-function', 'ease-out')
-				setTimeout(() => {
+		animateOnScroll()
+	}, [])
+
+	const animateOnScroll = async () => {
+		window.addEventListener(
+			'scroll',
+			async () => {
+				handleScrollOffset(async () => {
+					bannerRef.current.style.setProperty('transform', 'translate(70px)')
+					bannerRef.current.style.setProperty('transition-duration', '.3s')
+					bannerRef.current.style.setProperty('transition-timing-function', 'ease-out')
+
+					await waitForScrollEnd()
+
 					bannerRef.current.style.setProperty('transform', 'translate(-50px)')
 					bannerRef.current.style.setProperty('transition-duration', '.3s')
 					bannerRef.current.style.setProperty('transition-timing-function', 'ease-out')
-				}, 300)
+				}, 100)
+			},
+			false
+		)
+	}
+
+	const handleScrollOffset = (callback, refresh) => {
+		let isScrolling, start, end, distance
+
+		if (!start) {
+			start = window.scrollY
+		}
+
+		window.clearTimeout(isScrolling)
+
+		isScrolling = setTimeout(() => {
+			end = window.scrollY
+			distance = end - start
+
+			if (distance > 10) {
+				callback()
 			}
+		}, refresh)
+	}
+
+	const waitForScrollEnd = async () => {
+		let last_changed_frame = 0
+		let last_x = window.scrollX
+		let last_y = window.scrollY
+
+		return new Promise((resolve) => {
+			function tick(frames) {
+				if (frames >= 500 || frames - last_changed_frame > 100) {
+					resolve()
+				} else {
+					if (window.scrollX != last_x || window.scrollY != last_y) {
+						last_changed_frame = frames
+						last_x = window.scrollX
+						last_y = window.scrollY
+					}
+					requestAnimationFrame(tick.bind(null, frames + 1))
+				}
+			}
+			tick(0)
 		})
-	}, [])
+	}
 
 	if ((!data && isValidating) || data.length === 0) {
 		return null
@@ -36,12 +86,12 @@ const FloatingBanner = () => {
 				<Fragment>
 					<div
 						ref={bannerRef}
-						className="block md:hidden h-36 w-24 p-4 md:m-auto z-20 fixed -right-8 md:right-36 bottom-5 transform -translate-x-1/2 md:left-auto md:transform-none cursor-pointer"
+						className="flex justify-center items-center md:hidden h-32 w-32 z-20 fixed -right-8 md:right-36 bottom-5 transform -translate-x-1/2 cursor-pointer bg-black bg-opacity-50 rounded-full"
 					>
 						<Link href={`${data?.open_link}`}>
-							<div className="absolute right-0">
+							<div>
 								<div className="cursor-pointer">
-									<img src={parseImgUrl(data?.image)} />
+									<img src={parseImgUrl(data?.image)} className="w-[100px]" />
 								</div>
 							</div>
 						</Link>
