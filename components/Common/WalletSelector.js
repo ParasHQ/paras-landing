36ec638f208ceaf6341416c9f29d0e1b41a2e508
Muhlis Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2023-01-18 19:23:15 +0700

    fix: bugs on wallettype

--- components/Common/WalletSelector.js
+++ components/Common/WalletSelector.js
@@ -150,7 +150,7 @@ export const WalletSelectorContextProvider = ({ children }) => {
 			.subscribe(({ accounts, selectedWalletId }) => {
 				setSelectedWallet(selectedWalletId)
 				const accountId = accounts.find((account) => account.active)?.accountId || null
-				setupUser({ accountId: accountId }, selectedWalletId)
+				setupUser({ accountId: accountId })
 				loadDynamicWombiScript((WA) => {
 					if (accountId) WA.setWallet(accountId)
 				})
@@ -307,7 +307,40 @@ export const WalletSelectorContextProvider = ({ children }) => {
 
 		if (walletType === 'wallet-selector') {
 			const wallet = await selector.wallet()
-			signedMsg = await wallet.signMessage({ message: msgBuf })
+			if (wallet.id === 'here-wallet') {
+				const hereSignedMsg = JSON.parse(localStorage.getItem('HERE_SIGNED_MSG'))
+				if (hereSignedMsg && hereSignedMsg.accountId === accountId) {
+					signedMsg = hereSignedMsg.signedMsg
+				} else {
+					const wallet = await selector.wallet()
+					signedMsg = await wallet.signMessage({ message: msgBuf })
+				}
+
+				if (!signedMsg) {
+					signOut()
+					localStorage.removeItem('HERE_SIGNED_MSG')
+					localStorage.removeItem('PARAS_ACTIVE_WALLET')
+
+					window.location.replace(window.location.origin + window.location.pathname)
+					return
+				}
+
+				// save the signed message to local storage
+				const signedMsgString = JSON.stringify({ accountId, signedMsg })
+				localStorage.setItem('HERE_SIGNED_MSG', signedMsgString)
+
+				if (hereSignedMsg) {
+					signedMsg.publicKey.data = new Uint8Array(
+						Object.values(hereSignedMsg.signedMsg.publicKey.data.data)
+					)
+					signedMsg.signature = new Uint8Array(Object.values(hereSignedMsg.signedMsg.signature))
+				} else {
+					signedMsg.publicKey.data = new Uint8Array(Object.values(signedMsg.publicKey.data))
+					signedMsg.signature = new Uint8Array(Object.values(signedMsg.signature))
+				}
+			} else {
+				signedMsg = await wallet.signMessage({ message: msgBuf })
+			}
 		}
 
 		if (walletType === 'ramper') {
@@ -337,39 +370,6 @@ export const WalletSelectorContextProvider = ({ children }) => {
 
 			signedMsg.publicKey.data = new Uint8Array(Object.values(signedMsg.publicKey.data))
 			signedMsg.signature = new Uint8Array(Object.values(signedMsg.signature))
-		}
-
-		if (walletType === 'here-wallet') {
-			const hereSignedMsg = JSON.parse(localStorage.getItem('HERE_SIGNED_MSG'))
-			if (hereSignedMsg && hereSignedMsg.accountId === accountId) {
-				signedMsg = hereSignedMsg.signedMsg
-			} else {
-				const wallet = await selector.wallet()
-				signedMsg = await wallet.signMessage({ message: msgBuf })
-			}
-
-			if (!signedMsg) {
-				signOut()
-				localStorage.removeItem('HERE_SIGNED_MSG')
-				localStorage.removeItem('PARAS_ACTIVE_WALLET')
-
-				window.location.replace(window.location.origin + window.location.pathname)
-				return
-			}
-
-			// save the signed message to local storage
-			const signedMsgString = JSON.stringify({ accountId, signedMsg })
-			localStorage.setItem('HERE_SIGNED_MSG', signedMsgString)
-
-			if (hereSignedMsg) {
-				signedMsg.publicKey.data = new Uint8Array(
-					Object.values(hereSignedMsg.signedMsg.publicKey.data.data)
-				)
-				signedMsg.signature = new Uint8Array(Object.values(hereSignedMsg.signedMsg.signature))
-			} else {
-				signedMsg.publicKey.data = new Uint8Array(Object.values(signedMsg.publicKey.data))
-				signedMsg.signature = new Uint8Array(Object.values(signedMsg.signature))
-			}
 		}
 
 		const pubKey = Buffer.from(signedMsg.publicKey.data).toString('hex')
